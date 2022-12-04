104396017f7a23d2f545de5bd4506be5d0f38980
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-12-05 00:00:54 +0100

    add prelude to have every imports ready collectively

---
+++ src/prelude.rs
@@ -0,0 +1,17 @@
+pub use crate::cell_properties::cell_model::*;
+pub use crate::cell_properties::cycle::*;
+pub use crate::cell_properties::death::*;
+pub use crate::cell_properties::flags::*;
+pub use crate::cell_properties::interaction::*;
+pub use crate::cell_properties::mechanics::*;
+
+pub use crate::concepts::cell::*;
+pub use crate::concepts::cycle::*;
+pub use crate::concepts::domain::*;
+pub use crate::concepts::errors::*;
+pub use crate::concepts::interaction::*;
+pub use crate::concepts::mechanics::*;
+
+pub use crate::domain::cartesian_cuboid::*;
+
+pub use crate::plotting::cells_2d::*;
