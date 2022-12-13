212e1890e869b4b281284c5547734bdb8a06da2e
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-12-13 23:19:57 +0100

    use trait aliases for generics; first iteration of interface

--- src/concepts/cell.rs
+++ src/concepts/cell.rs
@@ -1,11 +1,13 @@
 use crate::concepts::cycle::*;
-use crate::concepts::mechanics::*;
+use crate::concepts::mechanics::{Position,Force,Velocity,Mechanics};
 use crate::concepts::interaction::*;
+
+use std::marker::Send;
 
 use uuid::Uuid;
 
 
-pub trait Cell<Pos, Force, Velocity> = Cycle<Self> + Interaction<Pos, Force> + Mechanics<Pos, Velocity> + Sized + Id;
+pub trait Cell<Pos: Position, For: Force, Vel: Velocity> = Cycle<Self> + Interaction<Pos, For> + Mechanics<Pos, For, Vel> + Sized + Id + Send;
 
 pub trait Id {
     fn get_uuid(&self) -> Uuid;
--- src/concepts/mechanics.rs
+++ src/concepts/mechanics.rs
@@ -1,12 +1,25 @@
+use crate::concepts::errors::CalcError;
+
+use core::fmt::Debug;
+use core::ops::{Add,AddAssign,Sub,SubAssign,Mul};
+use num::Zero;
+use std::marker::Send;
 
 
-pub trait Mechanics<Pos,Velocity> {
+pub trait Position = Sized + Add<Self,Output=Self> + AddAssign + Sub + SubAssign + Clone + Debug + Send + Mul<f64,Output=Self>;
+pub trait Force = Sized + Add<Self,Output=Self> + AddAssign + Sub + SubAssign + Debug + Zero + Send + Mul<f64,Output=Self>;
+pub trait Velocity = Sized + Add<Self,Output=Self> + AddAssign + Sub + SubAssign + Debug + Zero + Send + Mul<f64,Output=Self>;
+
+
+pub trait Mechanics<Pos, For, Vel>
+where
+    Pos: Position,
+    For: Force,
+    Vel: Velocity,
+{
     fn pos(&self) -> Pos;
-    fn velocity(&self) -> Velocity;
-
-    fn set_pos(&mut self, p: &Pos);
-    fn set_velocity(&mut self, v: &Velocity);
-
-    fn add_pos(&mut self, dp: &Pos);
-    fn add_velocity(&mut self, dv: &Velocity);
+    fn velocity(&self) -> Vel;
+    fn set_pos(&mut self, pos: &Pos);
+    fn set_velocity(&mut self, velocity: &Vel);
+    fn calculate_increment(&self, force: For) -> Result<(Pos, Vel), CalcError>;
 }
