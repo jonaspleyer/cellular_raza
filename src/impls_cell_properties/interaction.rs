849e1df25ae9fe6c1c414c5b60ca7219a52047c7
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-12-13 23:23:34 +0100

    cells may not produce a force at all

--- src/impls_cell_properties/interaction.rs
+++ src/impls_cell_properties/interaction.rs
@@ -13,16 +13,16 @@ pub struct LennardJones {
 
 
 impl Interaction<Vector3<f64>,Vector3<f64>> for LennardJones {
-    fn potential(&self, x: &Vector3<f64>, y: &Vector3<f64>) -> Result<Vector3<f64>, CalcError> {
-        let r = (x - y).norm();
-        let z = x - y;
+    fn force(&self, own_pos: &Vector3<f64>, ext_pos: &Vector3<f64>) -> Option<Result<Vector3<f64>, CalcError>> {
+        let z = own_pos - ext_pos;
+        let r = z.norm();
         let dir = z/r;
         let val = 4.0 * self.epsilon / r * (12.0 * (self.sigma/r).powf(12.0) - 1.0 * (self.sigma/r).powf(1.0));
         let max = 10.0 * self.epsilon / r;
         if val > max {
-            return Ok(dir * max);
+            return Some(Ok(dir * max));
         } else {
-            return Ok(dir * val);
+            return Some(Ok(dir * val));
         }
     }
 }
