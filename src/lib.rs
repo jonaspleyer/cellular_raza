6ea537152740921b6cbd40f29f4abb312be640e6
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-12-14 13:37:45 +0100

    comment modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 #![feature(drain_filter)]
 #![feature(trait_alias)]
 
+/// Semi-Abstract concepts for cells and simulation engine
 pub mod concepts;
+/// Example implementations of cell models
 pub mod impls_cell_models;
+/// Implementations of individual cell properties
 pub mod impls_cell_properties;
+/// Implementations of Domains
 pub mod impls_domain;
+/// Objects and Methods for initializing and controlling the simulation flow
 pub mod init;
+/// Methods for plotting simulation results
 pub mod plotting;
+/// Re-exports of all objects and methods
 pub mod prelude;
