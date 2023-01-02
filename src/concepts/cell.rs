a524ebc8de3c93acbbfda6657677828cb8407960
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-01-02 11:19:28 +0100

    do not use trait alias anymore due to compiler error

--- src/concepts/cell.rs
+++ src/concepts/cell.rs
@@ -7,7 +7,16 @@ use std::marker::{Send,Sync};
 use uuid::Uuid;
 
 
-pub trait Cell<Pos: Position, For: Force, Vel: Velocity> = Cycle<Self> + Interaction<Pos, For> + Mechanics<Pos, For, Vel> + Sized + Id + Send + Sync + Clone;
+// pub trait Cell<Pos: Position, For: Force, Vel: Velocity> = Cycle<Self> + Interaction<Pos, For> + Mechanics<Pos, For, Vel> + Sized + Id + Send + Sync + Clone;
+pub trait Cell<Pos: Position, For: Force, Vel: Velocity>: Cycle<Self> + Interaction<Pos, For> + Mechanics<Pos, For, Vel> + Sized + Id + Send + Sync + Clone {}
+impl<Pos, For, Vel, A> Cell<Pos, For, Vel> for A
+where
+    Pos: Position,
+    For: Force,
+    Vel: Velocity,
+    A: Cycle<Self> + Interaction<Pos, For> + Mechanics<Pos, For, Vel> + Sized + Id + Send + Sync + Clone,
+{}
+
 
 pub trait Id {
     fn get_uuid(&self) -> Uuid;
