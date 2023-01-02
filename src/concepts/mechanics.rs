df762a8c28ca85bd77822095d66289f715887964
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-01-03 00:29:13 +0100

    share position, force and velocity safely between threads

--- src/concepts/mechanics.rs
+++ src/concepts/mechanics.rs
@@ -3,12 +3,12 @@ use crate::concepts::errors::CalcError;
 use core::fmt::Debug;
 use core::ops::{Add,AddAssign,Sub,SubAssign,Mul};
 use num::Zero;
-use std::marker::Send;
+use std::marker::{Send,Sync};
 
 
-pub trait Position = Sized + Add<Self,Output=Self> + AddAssign + Sub<Output=Self> + SubAssign + Clone + Debug + Send + Mul<f64,Output=Self>;
-pub trait Force = Sized + Add<Self,Output=Self> + AddAssign + Sub<Output=Self> + SubAssign + Debug + Zero + Send + Mul<f64,Output=Self>;
-pub trait Velocity = Sized + Add<Self,Output=Self> + AddAssign + Sub<Output=Self> + SubAssign + Debug + Zero + Send + Mul<f64,Output=Self>;
+pub trait Position = Sized + Add<Self,Output=Self> + AddAssign + Sub<Output=Self> + SubAssign + Clone + Debug + Send + Sync + Mul<f64,Output=Self>;
+pub trait Force = Sized + Add<Self,Output=Self> + AddAssign + Sub<Output=Self> + SubAssign + Clone + Debug + Zero + Send + Sync + Mul<f64,Output=Self>;
+pub trait Velocity = Sized + Add<Self,Output=Self> + AddAssign + Sub<Output=Self> + SubAssign + Clone + Debug + Zero + Send + Sync + Mul<f64,Output=Self>;
 
 
 pub trait Mechanics<Pos, For, Vel>
