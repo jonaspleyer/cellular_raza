8e5282c3a7b6743fe4df99b4945833e716851f2b
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-02-04 05:30:57 +0100

    implemented the lennard jones potential for dimensions 1,2,3

--- src/impls_cell_properties/interaction.rs
+++ src/impls_cell_properties/interaction.rs
@@ -2,7 +2,7 @@ use crate::concepts::errors::CalcError;
 use crate::concepts::interaction::*;
 
 
-use nalgebra::Vector3;
+use nalgebra::SVector;
 use serde::{Serialize,Deserialize};
 
 
@@ -23,18 +23,22 @@ pub struct LennardJones {
     pub sigma: f64,
 }
 
-
-impl Interaction<Vector3<f64>,Vector3<f64>> for LennardJones {
-    fn force(&self, own_pos: &Vector3<f64>, ext_pos: &Vector3<f64>) -> Option<Result<Vector3<f64>, CalcError>> {
-        let z = own_pos - ext_pos;
-        let r = z.norm();
-        let dir = z/r;
-        let val = 4.0 * self.epsilon / r * (12.0 * (self.sigma/r).powf(12.0) - 1.0 * (self.sigma/r).powf(1.0));
-        let max = 10.0 * self.epsilon / r;
-        if val > max {
-            return Some(Ok(dir * max));
-        } else {
-            return Some(Ok(dir * val));
+macro_rules! implement_lennard_jones_nd(
+    ($dim:literal) =>  {
+        impl Interaction<SVector<f64, $dim>,SVector<f64, $dim>> for LennardJones {
+            fn force(&self, own_pos: &SVector<f64, $dim>, ext_pos: &SVector<f64, $dim>) -> Option<Result<SVector<f64, $dim>, CalcError>> {
+                let z = own_pos - ext_pos;
+                let r = z.norm();
+                let dir = z/r;
+                let val = 4.0 * self.epsilon / r * (12.0 * (self.sigma/r).powf(12.0) - 1.0 * (self.sigma/r).powf(1.0));
+                let max = 10.0 * self.epsilon / r;
+                return Some(Ok(dir * max.min(val)));
+            }
         }
     }
-}
+);
+
+
+implement_lennard_jones_nd!(1);
+implement_lennard_jones_nd!(2);
+implement_lennard_jones_nd!(3);
