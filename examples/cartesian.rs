e43daa8a0639c38d1c24400f727258f9c0423794
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-02-13 16:32:56 +0100

    adapt to new nalgebra version

--- examples/cartesian.rs
+++ examples/cartesian.rs
@@ -24,44 +24,44 @@ fn pad_state(mut state: DMatrix<i32>) -> DMatrix<i32>
 {
     // Pad left side
     // Remove columns if they are empty
-    while state.slice((0,0),(state.nrows(),2)).sum() == 0 {
+    while state.view((0,0),(state.nrows(),2)).sum() == 0 {
         state = state.remove_columns_at(&[0]);
     }
     // Add columns if there are lattice points near edge
-    if state.slice((0,0),(state.nrows(),1)).sum() > 0 {
+    if state.view((0,0),(state.nrows(),1)).sum() > 0 {
         state = state.insert_column(0, 0);
     }
 
     // Pad top side
     // Remove rows if they are empty
-    while state.slice((0,0),(2,state.ncols())).sum() == 0 {
+    while state.view((0,0),(2,state.ncols())).sum() == 0 {
         state = state.remove_rows_at(&[0]);
     }
     // Add columns if there are lattice points near edge
-    if state.slice((0,0),(1,state.ncols())).sum() > 0 {
+    if state.view((0,0),(1,state.ncols())).sum() > 0 {
         state = state.insert_row(0, 0);
     }
 
     // Pad right side
     // Remove columns if they are empty
-    while state.slice((0,state.ncols()-2),(state.nrows(),2)).sum() == 0 {
+    while state.view((0,state.ncols()-2),(state.nrows(),2)).sum() == 0 {
         let n = state.ncols();
         state = state.remove_columns_at(&[n-1]);
     }
     // Add columns if there are lattice points near edge
-    if state.slice((0,state.ncols()-1),(state.nrows(),1)).sum() > 0 {
+    if state.view((0,state.ncols()-1),(state.nrows(),1)).sum() > 0 {
         let n = state.ncols();
         state = state.insert_column(n, 0);
     }
 
     // Pad bottom side
     // Remove columns if they are empty
-    while state.slice((state.nrows()-2,0),(2,state.ncols())).sum() == 0 {
+    while state.view((state.nrows()-2,0),(2,state.ncols())).sum() == 0 {
         let n = state.nrows();
         state = state.remove_rows_at(&[n-1]);
     }
     // Add columns if there are lattice points near edge
-    if state.slice((state.nrows()-1,0),(1,state.ncols())).sum() > 0 {
+    if state.view((state.nrows()-1,0),(1,state.ncols())).sum() > 0 {
         let n = state.nrows();
         state = state.insert_row(n, 0);
     }
@@ -107,15 +107,15 @@ fn main() {
         d = &state * &d_m;
         sum = c+d;
         
-        let x_00 = state.slice((0, 0), (n-1, m-1));
-        let x_01 = state.slice((0, 1), (n-1, m-1));
-        let x_10 = state.slice((1, 0), (n-1, m-1));
-        let x_11 = state.slice((1, 1), (n-1, m-1));
+        let x_00 = state.view((0, 0), (n-1, m-1));
+        let x_01 = state.view((0, 1), (n-1, m-1));
+        let x_10 = state.view((1, 0), (n-1, m-1));
+        let x_11 = state.view((1, 1), (n-1, m-1));
         
-        sum.slice_mut((0, 0), (n-1, m-1)).add_assign(x_11);
-        sum.slice_mut((0, 1), (n-1, m-1)).add_assign(x_10);
-        sum.slice_mut((1, 0), (n-1, m-1)).add_assign(x_01);
-        sum.slice_mut((1, 1), (n-1, m-1)).add_assign(x_00);
+        sum.view_mut((0, 0), (n-1, m-1)).add_assign(x_11);
+        sum.view_mut((0, 1), (n-1, m-1)).add_assign(x_10);
+        sum.view_mut((1, 0), (n-1, m-1)).add_assign(x_01);
+        sum.view_mut((1, 1), (n-1, m-1)).add_assign(x_00);
 
         for (si, cdi) in state.iter_mut().zip((sum).iter_mut()) {
             if *si == 1 && *cdi < 2 {
