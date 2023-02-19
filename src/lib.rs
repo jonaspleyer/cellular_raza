2d9336548604059c2cbfac6040e6314884cf075e
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-02-19 21:32:01 +0100

    restructure description of crate
    - still needs lots of work and more details (not only overall
      description but crate itself)

--- src/lib.rs
+++ src/lib.rs
@@ -2,40 +2,50 @@
 #![feature(trait_alias)]
 
 //! This crate is an agent-based tool to simulate individual biological cells with a mechanistically driven mindset.
-//! This means, properties of cells are thought to be individually driven by local phenomena.
-//!
-//! This crate aims to offer the following features in the future
-//! - Intra- and Extracellular Reactions by (Stochastic) ODEs and PDEs
-// TODO extend list here: look at our own paper
-//! - Adaptive and Multi-Scale solving
-//! - Intra- and Extracellular Reactions via Gillespie and PDE+ODE solvers
-//! - Fluid Dynamics (Diffusion, etc.)
-//! - Provide predefined Set of Cellular Agents and Simulation Domains
-//! - Separation of Concepts and Implementations such that individual Implementations can be realized
-//! - Efficiently parallelized
-//! - Deterministic (once fixing initial Seed, if random Processes take place)
-//! - Python and Julia bindings
-//!
-//! What this crate does not aim to offer
-//! - A user graphical Interface
-//!     - Models should be created with native Rust Code or by interacting with Python or Julia
-//!     - Data Analysis can be done with Python/Julia as well
-//! - The option to freely change everything however you like during runtime
-//!
-//! # Simulating Cell-Biology
-//!
-//! # Concepts
+//! This means, properties of cells are thought to be individually driven by strictly local phenomena.
+//! # Simulating Cellular Biology
+//! This crate aims to provide interfaces to simulate the following mechanisms found in biological cells.
+//! - [x] Cellular mechanics via force interaction
+//! - [ ] Intra- and Extracellular Reactions by (Stochastic) ODEs and PDEs
+//! - [ ] Fluid Dynamics (Diffusion, etc.)
+//! - [ ] Cell-Proliferation
+//! - [ ] Death
+//! - [ ] Differentiation
+//! - [ ] Contact functions
+//! - [ ] Custom (individual-only) rules
 //!
 //! # Features and Techniques
+// TODO extend list here: look at our own paper
+//! - [ ] Adaptive and Multi-Scale solving
+//! - [ ] Provide predefined Set of Cellular Agents and Simulation Domains
+//! - [x] Separation of Concepts and Implementations
+//! - [ ] Provide predefined Set of Cellular Agents and Simulation Domains
+//! - [x] Parallelization
+//!     - [ ] Scheduling for highly-parallelized NUMA instances
+//! - [x] Deterministic (once fixing initial Seed, if random Processes take place)
+//! ## Concepts
+//! This crate provides well-defined traits to implement own cellular types with own rules.
+//!
 //! ## Simulation Snapshots
 //! - Take a simulation snapshot at any given time
 //! - Start from any saved Snapshot, even with possibly different parameter-values
 //! ## Deterministic Results
-//! - Results should be (if makeing corret use of the provided traits) deterministically reproducible, even when using parallelization
-//! ## Efficient Hardware Usage
-//! - Intrinsic parallelization over physical Simulation Domain
+//! This crate should always produce results which are deterministically reproducible.
+//! That is if the provided functionalities are used correctly (see [concepts]).
+//! Changing the number of threads should not alter the behaviour.
+//! ## Implementation Details
+//! - Intrinsic parallelization over physical simulation domain
+//! - Strongly prefer static dispatch (frequent use of generics)
+//! - Consider memory-locality and thread-to-thread latency
 // TODO
 //! - Efficient Cache-Usage
+//!
+//! # What this crate does not aim to offer
+//! - A user graphical Interface
+//!     - Models should be created with native Rust Code or by interacting with Python or Julia
+//!     - Data Analysis can be done with Python/Julia as well
+//! - The option to freely change everything however you like during runtime
+
 
 
 #[cfg(all(feature = "no_db", any(feature = "db_sled", feature = "db_mongodb")))]
