5d94371f9949d1d1b6b672c9c87ba82142477dcf
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-03-07 00:02:33 +0100

    first idea for pipeline trait

--- src/pipelines/mod.rs
+++ src/pipelines/mod.rs
@@ -1,3 +1,5 @@
+use crate::concepts::errors::SimulationError;
+
 /// Use multiple os-threads and cpu-only resources
 ///
 /// Parallelization is achieved by splitting the simualtion domain into as many chunks as threads are desired.
@@ -6,3 +8,15 @@
 /// The user can manage the simulation flow by means of individual funtions or by creating a
 /// [SimulationSupervisor](cpu_os_threads::supervisor::SimulationSupervisor).
 pub mod cpu_os_threads;
+
+pub trait Pipeline {
+    type Setup;
+    type SetupStrategies;
+    type SnapShot;
+
+    fn initialize(setup: Self::Setup) -> Self;
+    fn initialize_with_strategies(setup: Self::Setup, strategies: Self::SetupStrategies) -> Self;
+    fn initialize_from_full_snapshot(snapshot: Self::SnapShot) -> Self;
+
+    fn run_full_simulation(&mut self) -> Result<(), SimulationError>;
+}
