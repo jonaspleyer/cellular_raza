af8ec833728067f9eb3d151d8a88a2e6285a5bbe
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-03-26 13:45:55 +0200

    short overview of ModularCell

--- src/implementations/cell_models/mod.rs
+++ src/implementations/cell_models/mod.rs
@@ -1,3 +1,10 @@
 pub mod custom_cell_nd;
+
+/// This module contains a ModularCell which is used to define cell types from individual cellular
+/// properties such as mechanics, reactions, etc.
+///
+/// The [ModularCell](modular_cell::ModularCell) is a struct with fields that implement the various
+/// [concepts](crate::concepts). The concepts are afterwards derived automatically for the
+/// [ModularCell] struct.
 pub mod modular_cell;
 pub mod standard_cell_2d;
