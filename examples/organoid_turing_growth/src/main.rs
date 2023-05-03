f33f4db818ff8d7ee093607ab5f59a348f86e318
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-05-03 15:54:12 +0200

    mainly formatting - some last changes before generating movie for seminar talk

--- examples/organoid_turing_growth/src/main.rs
+++ examples/organoid_turing_growth/src/main.rs
@@ -1,13 +1,12 @@
-use cellular_raza::pipelines::cpu_os_threads::prelude::*;
+use cellular_raza::backend::cpu_os_threads::prelude::*;
 use cellular_raza::implementations::cell_models::modular_cell::ModularCell;
 
 use nalgebra::Vector2;
 
+use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
-use rand::{SeedableRng,Rng};
 
 use plotting::*;
-
 
 // Number of cells to put into simulation in the Beginning
 pub const N_CELLS_ORGANOID: u32 = 1000;
@@ -23,7 +22,7 @@ pub const CELL_MECHANICS_VELOCITY_REDUCTION: f64 = 2.0;
 pub const CELL_BMP4_INITIAL_CONCENTRATION: f64 = 0.0;
 pub const CELL_BMP4_PRODUCTION_RATE: f64 = 0.05;
 pub const CELL_BMP4_DEGRADATION_RATE: f64 = 0.005;
-pub const CELL_BMP4_SATURATION: f64 = CELL_BMP4_PRODUCTION_RATE/CELL_BMP4_DEGRADATION_RATE;
+pub const CELL_BMP4_SATURATION: f64 = CELL_BMP4_PRODUCTION_RATE / CELL_BMP4_DEGRADATION_RATE;
 pub const CELL_BMP4_SECRETION_RATE: f64 = 0.1;
 pub const CELL_BMP4_UPTAKE_RATE: f64 = 0.1;
 
@@ -35,11 +34,11 @@ pub const CELL_FOOD_UPTAKE_RATE: f64 = 0.02;
 
 // Parameters for cell turing pattern
 pub const CELL_TURING_PATTERN_K1: f64 = 10.0;
-pub const CELL_TURING_PATTERN_K2: f64 =  0.1;
+pub const CELL_TURING_PATTERN_K2: f64 = 0.1;
 pub const CELL_TURING_PATTERN_K3: f64 = 2.938271604938272e-07;
 pub const CELL_TURING_PATTERN_K4: f64 = 80.0;
 
-pub const VOXEL_TURING_PATTERN_DIFFUSION_CONSTANT_1: f64 =   50.0;
+pub const VOXEL_TURING_PATTERN_DIFFUSION_CONSTANT_1: f64 = 50.0;
 pub const VOXEL_TURING_PATTERN_DIFFUSION_CONSTANT_2: f64 = 2500.0;
 
 // Parameters for cell cycle
@@ -55,15 +54,15 @@ pub const DOMAIN_SIZE_X: f64 = 2500.0;
 pub const DOMAIN_SIZE_Y: f64 = 2500.0;
 
 // Where will the cells be placed initially
-pub const STARTING_DOMAIN_X_LOW:  f64 = DOMAIN_SIZE_X/2.0 - 250.0;
-pub const STARTING_DOMAIN_X_HIGH: f64 = DOMAIN_SIZE_X/2.0 + 250.0;
-pub const STARTING_DOMAIN_Y_LOW:  f64 = DOMAIN_SIZE_Y/2.0 - 250.0;
-pub const STARTING_DOMAIN_Y_HIGH: f64 = DOMAIN_SIZE_Y/2.0 + 250.0;
+pub const STARTING_DOMAIN_X_LOW: f64 = DOMAIN_SIZE_X / 2.0 - 250.0;
+pub const STARTING_DOMAIN_X_HIGH: f64 = DOMAIN_SIZE_X / 2.0 + 250.0;
+pub const STARTING_DOMAIN_Y_LOW: f64 = DOMAIN_SIZE_Y / 2.0 - 250.0;
+pub const STARTING_DOMAIN_Y_HIGH: f64 = DOMAIN_SIZE_Y / 2.0 + 250.0;
 
 // Place the sender cells at the bottom of the simulation
-pub const STARTING_DOMAIN_X_SENDER_LOW:  f64 = DOMAIN_SIZE_X/2.0 - 500.0;
-pub const STARTING_DOMAIN_X_SENDER_HIGH: f64 = DOMAIN_SIZE_X/2.0 + 500.0;
-pub const STARTING_DOMAIN_Y_SENDER_LOW:  f64 = DOMAIN_SIZE_Y - 100.0;
+pub const STARTING_DOMAIN_X_SENDER_LOW: f64 = DOMAIN_SIZE_X / 2.0 - 500.0;
+pub const STARTING_DOMAIN_X_SENDER_HIGH: f64 = DOMAIN_SIZE_X / 2.0 + 500.0;
+pub const STARTING_DOMAIN_Y_SENDER_LOW: f64 = DOMAIN_SIZE_Y - 100.0;
 pub const STARTING_DOMAIN_Y_SENDER_HIGH: f64 = DOMAIN_SIZE_Y;
 
 // Parameters for Voxel Reaction+Diffusion
@@ -81,14 +80,12 @@ pub const N_TIMES: usize = 100_000;
 pub const DT: f64 = 0.01;
 pub const T_START: f64 = 0.0;
 pub const SAVE_INTERVAL: usize = 500;
-pub const FULL_SAVE_INTERVAL: usize = 500;
 
 // Meta Parameters to control solving
 pub const N_THREADS: usize = 2;
 
-
-mod plotting;
 mod cell_properties;
+mod plotting;
 
 use cell_properties::*;
 
@@ -111,23 +108,16 @@ fn voxel_definition_strategy(voxel: &mut CartesianCuboidVoxel2<NUMBER_OF_REACTIO
         0.0,
         0.0,
     ]);
-    voxel.production_rate = ReactionVector::from([
-        0.0,
-        VOXEL_FOOD_PRODUCTION_RATE,
-        0.0,
-        0.0,
-    ]);
+    voxel.production_rate = ReactionVector::from([0.0, VOXEL_FOOD_PRODUCTION_RATE, 0.0, 0.0]);
 }
-
 
 fn create_domain() -> Result<CartesianCuboid2, CalcError> {
     CartesianCuboid2::from_boundaries_and_interaction_ranges(
         [0.0; 2],
         [DOMAIN_SIZE_X, DOMAIN_SIZE_Y],
-        [CELL_MECHANICS_RADIUS*CELL_MECHANICS_RELATIVE_INTERACTION_RANGE*2.0; 2],
+        [CELL_MECHANICS_RADIUS * CELL_MECHANICS_RELATIVE_INTERACTION_RANGE * 2.0; 2],
     )
 }
-
 
 fn main() {
     // Fix random seed
@@ -137,156 +127,159 @@ fn main() {
     let domain = create_domain().unwrap();
 
     // ###################################### DEFINE CELLS IN SIMULATION ######################################
-    let mut cells = (0..N_CELLS_ORGANOID as i32).map(|_| {
-        let x = rng.gen_range(STARTING_DOMAIN_X_LOW..STARTING_DOMAIN_X_HIGH);
-        let y = rng.gen_range(STARTING_DOMAIN_Y_LOW..STARTING_DOMAIN_Y_HIGH);
+    let mut cells = (0..N_CELLS_ORGANOID as i32)
+        .map(|_| {
+            let x = rng.gen_range(STARTING_DOMAIN_X_LOW..STARTING_DOMAIN_X_HIGH);
+            let y = rng.gen_range(STARTING_DOMAIN_Y_LOW..STARTING_DOMAIN_Y_HIGH);
 
-        let pos = Vector2::from([x, y]);
-        ModularCell {
-            mechanics: MechanicsModel2D {
-                pos,
-                vel: Vector2::from([0.0, 0.0]),
-                dampening_constant: CELL_MECHANICS_VELOCITY_REDUCTION,
-            },
-            interaction: CellSpecificInteraction {
-                potential_strength: CELL_MECHANICS_POTENTIAL_STRENGTH,
-                relative_interaction_range: CELL_MECHANICS_RELATIVE_INTERACTION_RANGE,
-                cell_radius: CELL_MECHANICS_RADIUS,
-            },
-            interaction_extracellular: GradientSensing {},
-            cycle: OwnCycle::new(
-                rng.gen_range(0.0..CELL_CYCLE_DIVISION_AGE_MAX),
-                CELL_MECHANICS_RADIUS,
-                CELL_CYCLE_GROWTH_RATE,
-                CELL_CYCLE_FOOD_GROWTH_RATE_MULTIPLIER,
-                CELL_CYCLE_FOOD_DEATH_THRESHOLD,
-                CELL_CYCLE_FOOD_DIVISION_THRESHOLD,
-                true
-            ),
-            cellular_reactions: OwnReactions {
-                intracellular_concentrations: ReactionVector::from([
-                    0.0,
-                    CELL_FOOD_INITIAL_CONCENTRATION,
-                    rng.gen_range(200.0..500.0),
-                    rng.gen_range(200.0..500.0)
-                ]),
-                intracellular_concentrations_saturation_level: ReactionVector::from([
-                    CELL_BMP4_SATURATION,
-                    CELL_FOOD_SATURATION,
-                    0.0,
-                    0.0,
-                ]),
-                production_term: ReactionVector::from([
-                    0.0,
-                    -CELL_FOOD_CONSUMPTION_RATE,
-                    0.0,
-                    0.0,
-                ]),
-                degradation_rate: ReactionVector::from([
-                    CELL_BMP4_DEGRADATION_RATE,
-                    0.0,
-                    0.01,
-                    0.0,
-                ]),
-                secretion_rate: ReactionVector::from([
-                    0.0,
-                    CELL_FOOD_SECRETION_RATE,
-                    100.0,
-                    100.0,
-                ]),
-                uptake_rate: ReactionVector::from([
-                    CELL_BMP4_UPTAKE_RATE,
-                    CELL_FOOD_UPTAKE_RATE,
-                    100.0,
-                    100.0,
-                ]),
+            let pos = Vector2::from([x, y]);
+            ModularCell {
+                mechanics: MechanicsModel2D {
+                    pos,
+                    vel: Vector2::from([0.0, 0.0]),
+                    dampening_constant: CELL_MECHANICS_VELOCITY_REDUCTION,
+                },
+                interaction: CellSpecificInteraction {
+                    potential_strength: CELL_MECHANICS_POTENTIAL_STRENGTH,
+                    relative_interaction_range: CELL_MECHANICS_RELATIVE_INTERACTION_RANGE,
+                    cell_radius: CELL_MECHANICS_RADIUS,
+                },
+                interaction_extracellular: GradientSensing {},
+                cycle: OwnCycle::new(
+                    rng.gen_range(0.0..CELL_CYCLE_DIVISION_AGE_MAX),
+                    CELL_MECHANICS_RADIUS,
+                    CELL_CYCLE_GROWTH_RATE,
+                    CELL_CYCLE_FOOD_GROWTH_RATE_MULTIPLIER,
+                    CELL_CYCLE_FOOD_DEATH_THRESHOLD,
+                    CELL_CYCLE_FOOD_DIVISION_THRESHOLD,
+                    true,
+                ),
+                cellular_reactions: OwnReactions {
+                    intracellular_concentrations: ReactionVector::from([
+                        0.0,
+                        CELL_FOOD_INITIAL_CONCENTRATION,
+                        rng.gen_range(200.0..500.0),
+                        rng.gen_range(200.0..500.0),
+                    ]),
+                    intracellular_concentrations_saturation_level: ReactionVector::from([
+                        CELL_BMP4_SATURATION,
+                        CELL_FOOD_SATURATION,
+                        0.0,
+                        0.0,
+                    ]),
+                    production_term: ReactionVector::from([
+                        0.0,
+                        -CELL_FOOD_CONSUMPTION_RATE,
+                        0.0,
+                        0.0,
+                    ]),
+                    degradation_rate: ReactionVector::from([
+                        CELL_BMP4_DEGRADATION_RATE,
+                        0.0,
+                        0.01,
+                        0.0,
+                    ]),
+                    secretion_rate: ReactionVector::from([
+                        0.0,
+                        CELL_FOOD_SECRETION_RATE,
+                        100.0,
+                        100.0,
+                    ]),
+                    uptake_rate: ReactionVector::from([
+                        CELL_BMP4_UPTAKE_RATE,
+                        CELL_FOOD_UPTAKE_RATE,
+                        100.0,
+                        100.0,
+                    ]),
 
-                p1: CELL_TURING_PATTERN_K1,
-                p2: CELL_TURING_PATTERN_K2,
-                p3: CELL_TURING_PATTERN_K3,
-                p4: CELL_TURING_PATTERN_K4,
+                    p1: CELL_TURING_PATTERN_K1,
+                    p2: CELL_TURING_PATTERN_K2,
+                    p3: CELL_TURING_PATTERN_K3,
+                    p4: CELL_TURING_PATTERN_K4,
 
-                bmp4_hill: 0.00,
+                    bmp4_hill: 0.00,
+                },
             }
-        }}).collect::<Vec<_>>();
+        })
+        .collect::<Vec<_>>();
 
-    let mut cells2 = (0..N_CELLS_SENDER).map(|_| {
-        let x = rng.gen_range(STARTING_DOMAIN_X_SENDER_LOW..STARTING_DOMAIN_X_SENDER_HIGH);
-        let y = rng.gen_range(STARTING_DOMAIN_Y_SENDER_LOW..STARTING_DOMAIN_Y_SENDER_HIGH);
+    let mut cells2 = (0..N_CELLS_SENDER)
+        .map(|_| {
+            let x = rng.gen_range(STARTING_DOMAIN_X_SENDER_LOW..STARTING_DOMAIN_X_SENDER_HIGH);
+            let y = rng.gen_range(STARTING_DOMAIN_Y_SENDER_LOW..STARTING_DOMAIN_Y_SENDER_HIGH);
 
-        let pos = Vector2::from([x, y]);
-        ModularCell {
-            mechanics: MechanicsModel2D {
-                pos,
-                vel: Vector2::from([0.0, 0.0]),
-                dampening_constant: CELL_MECHANICS_VELOCITY_REDUCTION,
-            },
-            interaction: CellSpecificInteraction {
-                potential_strength: CELL_MECHANICS_POTENTIAL_STRENGTH,
-                relative_interaction_range: CELL_MECHANICS_RELATIVE_INTERACTION_RANGE,
-                cell_radius: CELL_MECHANICS_RADIUS,
-            },
-            interaction_extracellular: GradientSensing {},
-            cycle: OwnCycle::new(
-                rng.gen_range(0.0..CELL_CYCLE_DIVISION_AGE_MAX),
-                CELL_MECHANICS_RADIUS,
-                0.0,
-                CELL_CYCLE_FOOD_GROWTH_RATE_MULTIPLIER,
-                CELL_CYCLE_FOOD_DEATH_THRESHOLD,
-                1000.0,
-                true
-            ),
-            cellular_reactions: OwnReactions {
-                intracellular_concentrations: ReactionVector::from([
+            let pos = Vector2::from([x, y]);
+            ModularCell {
+                mechanics: MechanicsModel2D {
+                    pos,
+                    vel: Vector2::from([0.0, 0.0]),
+                    dampening_constant: CELL_MECHANICS_VELOCITY_REDUCTION,
+                },
+                interaction: CellSpecificInteraction {
+                    potential_strength: CELL_MECHANICS_POTENTIAL_STRENGTH,
+                    relative_interaction_range: CELL_MECHANICS_RELATIVE_INTERACTION_RANGE,
+                    cell_radius: CELL_MECHANICS_RADIUS,
+                },
+                interaction_extracellular: GradientSensing {},
+                cycle: OwnCycle::new(
+                    rng.gen_range(0.0..CELL_CYCLE_DIVISION_AGE_MAX),
+                    CELL_MECHANICS_RADIUS,
                     0.0,
-                    CELL_FOOD_INITIAL_CONCENTRATION,
-                    rng.gen_range(200.0..500.0),
-                    rng.gen_range(200.0..500.0)
-                ]),
-                intracellular_concentrations_saturation_level: ReactionVector::from([
-                    CELL_BMP4_SATURATION,
-                    CELL_FOOD_SATURATION,
-                    0.0,
-                    0.0,
-                ]),
-                production_term: ReactionVector::from([
-                    CELL_BMP4_PRODUCTION_RATE,
-                    -CELL_FOOD_CONSUMPTION_RATE,
-                    0.0,
-                    0.0,
-                ]),
-                degradation_rate: ReactionVector::from([
-                    0.0,
-                    0.0,
-                    0.01,
-                    0.0,
-                ]),
-                secretion_rate: ReactionVector::from([
-                    CELL_BMP4_SECRETION_RATE,
-                    CELL_FOOD_SECRETION_RATE,
-                    0.0,
-                    0.0,
-                ]),
-                uptake_rate: ReactionVector::from([
-                    CELL_BMP4_UPTAKE_RATE,
-                    CELL_FOOD_UPTAKE_RATE,
-                    0.0,
-                    0.0,
-                ]),
+                    CELL_CYCLE_FOOD_GROWTH_RATE_MULTIPLIER,
+                    CELL_CYCLE_FOOD_DEATH_THRESHOLD,
+                    1000.0,
+                    true,
+                ),
+                cellular_reactions: OwnReactions {
+                    intracellular_concentrations: ReactionVector::from([
+                        0.0,
+                        CELL_FOOD_INITIAL_CONCENTRATION,
+                        rng.gen_range(200.0..500.0),
+                        rng.gen_range(200.0..500.0),
+                    ]),
+                    intracellular_concentrations_saturation_level: ReactionVector::from([
+                        CELL_BMP4_SATURATION,
+                        CELL_FOOD_SATURATION,
+                        0.0,
+                        0.0,
+                    ]),
+                    production_term: ReactionVector::from([
+                        CELL_BMP4_PRODUCTION_RATE,
+                        -CELL_FOOD_CONSUMPTION_RATE,
+                        0.0,
+                        0.0,
+                    ]),
+                    degradation_rate: ReactionVector::from([0.0, 0.0, 0.01, 0.0]),
+                    secretion_rate: ReactionVector::from([
+                        CELL_BMP4_SECRETION_RATE,
+                        CELL_FOOD_SECRETION_RATE,
+                        0.0,
+                        0.0,
+                    ]),
+                    uptake_rate: ReactionVector::from([
+                        CELL_BMP4_UPTAKE_RATE,
+                        CELL_FOOD_UPTAKE_RATE,
+                        0.0,
+                        0.0,
+                    ]),
 
-                p1: 0.0,
-                p2: 0.0,
-                p3: 0.0,
-                p4: 0.0,
+                    p1: 0.0,
+                    p2: 0.0,
+                    p3: 0.0,
+                    p4: 0.0,
 
-                bmp4_hill: 0.02,
+                    bmp4_hill: 0.02,
+                },
             }
-    }}).collect::<Vec<_>>();
+        })
+        .collect::<Vec<_>>();
     cells.append(&mut cells2);
 
     // ###################################### CREATE SUPERVISOR AND RUN SIMULATION ######################################
     // Solve the initial steps more precisely when the Turint pattern is still forming
-    let t_eval = (0..N_TIMES).map(|n| (T_START + n as f64 * DT, n % SAVE_INTERVAL == 0, n % FULL_SAVE_INTERVAL == 0)).collect::<Vec<(f64, bool, bool)>>();
+    let t_eval = (0..N_TIMES)
+        .map(|n| (T_START + n as f64 * DT, n % SAVE_INTERVAL == 0))
+        .collect::<Vec<(f64, bool)>>();
 
     let setup = SimulationSetup {
         domain,
@@ -296,11 +289,11 @@ fn main() {
             t_eval,
         },
         meta_params: SimulationMetaParams {
-            n_threads: N_THREADS
+            n_threads: N_THREADS,
         },
         storage: StorageConfig {
             location: "out/organoid_turing_growth".to_owned().into(),
-        }
+        },
     };
 
     let strategies = Strategies {
@@ -318,8 +311,7 @@ fn main() {
         image_type: ImageType::BitMap,
     };
 
-    simulation_result.plot_spatial_all_iterations_custom_cell_voxel_functions(
-        &plot_modular_cell,
-        &plot_voxel
-    ).unwrap();
+    simulation_result
+        .plot_spatial_all_iterations_custom_cell_voxel_functions(&plot_modular_cell, &plot_voxel)
+        .unwrap();
 }
--- examples/organoid_turing_growth/src/plotting.rs
+++ examples/organoid_turing_growth/src/plotting.rs
@@ -1,9 +1,9 @@
-use cellular_raza::pipelines::cpu_os_threads::prelude::*;
+use cellular_raza::backend::cpu_os_threads::prelude::*;
 
 use plotters::{
-    prelude::{DrawingArea,Cartesian2d,Circle,ShapeStyle},
-    coord::types::RangedCoordf64,
     backend::BitMapBackend,
+    coord::types::RangedCoordf64,
+    prelude::{Cartesian2d, Circle, DrawingArea, ShapeStyle},
 };
 
 use nalgebra::Vector2;
@@ -22,30 +22,42 @@ pub fn plot_voxel(
     // This should give a nice colormap
     let voxel_color = ViridisRGB::get_color_normalized(concentration, lower_bound, upper_bound);
     let rectangle = plotters::prelude::Rectangle::new(
-        [(voxel.get_min()[0], voxel.get_min()[1]), (voxel.get_max()[0], voxel.get_max()[1])],
-        Into::<ShapeStyle>::into(&voxel_color).filled()
+        [
+            (voxel.get_min()[0], voxel.get_min()[1]),
+            (voxel.get_max()[0], voxel.get_max()[1]),
+        ],
+        Into::<ShapeStyle>::into(&voxel_color).filled(),
     );
     root.draw(&rectangle)?;
 
     // Also plot the direction in which the current concentration is pointing
     let gradient = voxel.extracellular_gradient[2];
     let strength = gradient.norm();
-    let gradient_upper_bound = (upper_bound-lower_bound)/voxel.get_dx().iter().sum::<f64>()*2.0;
+    let gradient_upper_bound =
+        (upper_bound - lower_bound) / voxel.get_dx().iter().sum::<f64>() * 2.0;
     let start = Vector2::from(voxel.get_middle());
-    let end = if gradient!=Vector2::from([0.0; 2]) {start + (gradient/strength.max(gradient_upper_bound)).component_mul(&Vector2::from(voxel.get_dx()))/2.0} else {start};
+    let end = if gradient != Vector2::from([0.0; 2]) {
+        start
+            + (gradient / strength.max(gradient_upper_bound))
+                .component_mul(&Vector2::from(voxel.get_dx()))
+                / 2.0
+    } else {
+        start
+    };
     let pointer = plotters::element::PathElement::new(
-        [(start.x, start.y),
-        (end.x, end.y)],
-        Into::<ShapeStyle>::into(&plotters::prelude::BLACK).filled().stroke_width(2),
+        [(start.x, start.y), (end.x, end.y)],
+        Into::<ShapeStyle>::into(&plotters::prelude::BLACK)
+            .filled()
+            .stroke_width(2),
     );
     root.draw(&pointer)?;
     Ok(())
 }
 
-
-pub fn plot_modular_cell
-    (modular_cell: &MyCellType, root: &mut DrawingArea<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>) -> Result<(), DrawingError>
-{
+pub fn plot_modular_cell(
+    modular_cell: &MyCellType,
+    root: &mut DrawingArea<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
+) -> Result<(), DrawingError> {
     let cell_border_color = plotters::prelude::BLACK;
 
     let relative_border_thickness = 0.25;
@@ -64,13 +76,25 @@ pub fn plot_modular_cell
 
     // Plot the inside of the cell
     let cell_inside_color = match modular_cell.cycle.is_ureter {
-        true => Life::get_color_normalized(modular_cell.get_intracellular()[1], 0.0, modular_cell.cellular_reactions.intracellular_concentrations_saturation_level[1]),
-        false => Poison::get_color_normalized(modular_cell.get_intracellular()[1], 0.0, modular_cell.cellular_reactions.intracellular_concentrations_saturation_level[1]),
+        true => Life::get_color_normalized(
+            modular_cell.get_intracellular()[1],
+            0.0,
+            modular_cell
+                .cellular_reactions
+                .intracellular_concentrations_saturation_level[1],
+        ),
+        false => Poison::get_color_normalized(
+            modular_cell.get_intracellular()[1],
+            0.0,
+            modular_cell
+                .cellular_reactions
+                .intracellular_concentrations_saturation_level[1],
+        ),
     };
     // let cell_inside_color = Life::get_color_normalized(modular_cell.get_intracellular()[1], 0.0, modular_cell.cellular_reactions.intracellular_concentrations_saturation_level[1]);
     let cell_inside = Circle::new(
         (modular_cell.pos().x, modular_cell.pos().y),
-        s*(1.0 - relative_border_thickness),
+        s * (1.0 - relative_border_thickness),
         Into::<ShapeStyle>::into(&cell_inside_color).filled(),
     );
     root.draw(&cell_inside)?;
