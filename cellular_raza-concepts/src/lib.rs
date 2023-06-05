8f99397868c411a59807f063a4d0ac49aecfcea0
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-06-05 17:06:16 +0200

    remove invalid references and describe otherwise

--- cellular_raza-concepts/src/lib.rs
+++ cellular_raza-concepts/src/lib.rs
@@ -105,20 +105,21 @@ pub mod domain;
 
 /// Error types which can arise during the simulation.
 ///
-/// There is one main error type [SimulationError](errors::SimulationError) which should be derivable from errors
+/// For Backends it may be useful to define a singular error type (eg. `SimulationError`) which should be derivable from errors
 /// arising during the simulation process.
 /// It is required for custom error types `MyCustomError` of the engine to implement the `From<MyCustomError> for SimulationError`.
 /// Errors should be seperated by their ability to be recovered, ignored or handled otherwise.
 /// Since this crate aims to provide an adaptive solving aproach, it is desired to have a fallback
 /// mechanism which can be called for errors which may arise due to precision problems.
 ///
-/// The following table shows a summary of the errors currently supported and their handling
-/// aproach.
+/// The following table shows a summary of the errors currently supported.
+/// Backends need to be aware of them and implement custom handling schemes to overcome or work around them.
+/// Aborting the simulation is an option but must be documented well without introducing undefined behaviour.
 ///
-/// | ErrorType | Possible Error Reasons | Handling Strategy |
+/// | ErrorType | Possible Error Reasons |
 /// | --- | --- | --- |
-/// | BoundaryError | Solver Accuracy, Domain Implementaion bug, Internal engine error | [RevertChangeAccuracy](errors::HandlingStrategies::RevertChangeAccuracy) |
-/// | CalcError | Solver Accuracy, Bug by user implementation of corresponding function, Internal engine error | [RevertChangeAccuracy](errors::HandlingStrategies::RevertChangeAccuracy) |
+/// | BoundaryError | Solver Accuracy, Domain Implementaion bug, Internal engine error |
+/// | CalcError | Solver Accuracy, Bug by user implementation of corresponding function, Internal engine error |
 // TODO implement the handling of these errors!
 pub mod errors;
 
@@ -148,4 +149,5 @@ pub mod mechanics;
 
 pub mod plotting;
 
+/// Exports which can be used to simply access all concepts.
 pub mod prelude;
