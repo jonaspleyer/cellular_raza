0696ba46f23039322d4cc68345e7e76a6f614a76
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-06-22 05:46:53 +0200

    add short description for benchmarks

--- cellular_raza-benchmarks/src/lib.rs
+++ cellular_raza-benchmarks/src/lib.rs
@@ -1,1 +1,5 @@
-
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+//! Contains Benchmarks surrounding `cellular_raza`
+//!
+//! We use [criterion] to automatically generate plots and retrieve information.
