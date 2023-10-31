d3a9f991162639d2be9ee3542580771f43983991
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-10-31 21:06:31 +0100

    further evolve derive macro

--- cellular_raza-concepts-derive/src/lib.rs
+++ cellular_raza-concepts-derive/src/lib.rs
@@ -1,11 +1,11 @@
-#![warn(missing_docs)]
-#![warn(clippy::missing_docs_in_private_items)]
+// #![warn(missing_docs)]
+// #![warn(clippy::missing_docs_in_private_items)]
 
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(CellAgent, attributes(Cycle, Mechanics, Interaction,))]
+#[proc_macro_derive(CellAgent, attributes(Cycle, Mechanics, Interaction, CellularReactions,InteractionExtracellularGradient))]
 pub fn my_macro(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let ast = parse_macro_input!(input as DeriveInput);
@@ -114,7 +114,7 @@ pub fn my_macro(input: TokenStream) -> TokenStream {
                 None => syn::parse2(quote!(())).unwrap(),
             };
             let res = quote! {
-                impl Interaction<#position, #velocity, #force, #inf> for #struct_name #struct_generics {
+                impl #struct_generics Interaction<#position, #velocity, #force, #inf> for #struct_name #struct_generics {
                     fn get_interaction_information(&self) -> #inf {
                         self.#name.get_interaction_information()
                     }
@@ -131,6 +131,73 @@ pub fn my_macro(input: TokenStream) -> TokenStream {
                     }
 
                     // fn contact_function(&mut self, other_cell: &C, environment: &mut Env) -> Result<(), SimulationError>;
+                }
+            };
+            result.extend(TokenStream::from(res));
+        } else if let Some(list) = field.attrs.iter().find_map(|x| match &x.meta {
+            syn::Meta::List(list) => {
+                if list.path.is_ident("CellularReactions") {
+                    Some(list)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }) {
+            let name = field.ident.clone();
+            let tokens = list.tokens.clone();
+            let stream = quote!((#tokens));
+            let attr: syn::TypeTuple = syn::parse2(stream).unwrap();
+            let mut elems = attr.elems.into_iter();
+            let concvecintracellular = elems.next().unwrap();
+            let concvecextracellular = elems.next().or_else(|| Some(concvecintracellular.clone())).unwrap();
+            let res = quote! {
+                impl #struct_generics CellularReactions<#concvecintracellular, #concvecextracellular> for #struct_name #struct_generics {
+                    fn get_intracellular(&self) -> #concvecintracellular {
+                        self.#name.get_intracellular()
+                    }
+
+                    fn set_intracellular(&mut self, concentration_vector: #concvecintracellular) {
+                        self.#name.set_intracellular(concentration_vector);
+                    }
+
+                    fn calculate_intra_and_extracellular_reaction_increment(
+                        &self,
+                        internal_concentration_vector: &#concvecintracellular,
+                        external_concentration_vector: &#concvecextracellular,
+                    ) -> Result<(#concvecintracellular, #concvecextracellular), CalcError> {
+                        self.#name.calculate_intra_and_extracellular_reaction_increment(
+                            internal_concentration_vector,
+                            external_concentration_vector
+                        )
+                    }
+                }
+            };
+            result.extend(TokenStream::from(res));
+        } else if let Some(list) = field.attrs.iter().find_map(|x| match &x.meta {
+            syn::Meta::List(list) => {
+                if list.path.is_ident("InteractionExtracellularGradient") {
+                    Some(list)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }) {
+            let field_type = &field.ty;
+            let tokens = list.tokens.clone();
+            let stream = quote!((#tokens));
+            let attr: syn::TypeTuple = syn::parse2(stream).unwrap();
+            let mut elems = attr.elems.into_iter();
+            let concgradientextracellular = elems.next().unwrap();
+            let res = quote! {
+                impl InteractionExtracellularGradient<#struct_name #struct_generics, #concgradientextracellular> for #struct_name #struct_generics {
+                    fn sense_gradient(
+                        cell: &mut #struct_name #struct_generics,
+                        gradient: &#concgradientextracellular,
+                    ) -> Result<(), CalcError> {
+                        #field_type::sense_gradient(cell, gradient)
+                    }
                 }
             };
             result.extend(TokenStream::from(res));
