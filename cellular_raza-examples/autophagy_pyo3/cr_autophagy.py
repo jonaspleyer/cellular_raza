6f8da8c93c47a7195952e74e07e29564336249a8
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-11-22 02:01:37 +0100

    use library file to collect shared functionality
    - also use more efficient way of retrieving results
    - especially necessary vor extremely large simulations

---
+++ cellular_raza-examples/autophagy_pyo3/cr_autophagy.py
@@ -0,0 +1,51 @@
+import os
+import json
+import pandas as pd
+from pathlib import Path
+from cr_autophagy_pyo3 import *
+import multiprocessing as mp
+import numpy as np
+
+def get_last_output_path(name = "autophagy"):
+    return Path("out") / name / sorted(os.listdir(Path("out") / name))[-1]
+
+def _combine_batches(run_directory):
+    # Opens all batches in a given directory and stores
+    # them in one unified big list
+    combined_batch = []
+    for batch_file in os.listdir(run_directory):
+        f = open(run_directory / batch_file)
+        b = json.load(f)["data"]
+        combined_batch.extend(b)
+    return combined_batch
+
+def get_particles_at_iter(output_path, iteration):
+    dir = Path(output_path) / "cell_storage/json"
+    run_directory = None
+    for x in os.listdir(dir):
+        if int(x) == iteration:
+            run_directory = dir / x
+            break
+    if run_directory != None:
+        df = pd.json_normalize(_combine_batches(run_directory))
+        df["identifier"] = df["identifier"].apply(lambda x: tuple(x))
+        df["element.cell.mechanics.mechanics.pos"] = df["element.cell.mechanics.mechanics.pos"].apply(lambda x: np.array(x, dtype=float))
+        df["element.cell.mechanics.mechanics.vel"] = df["element.cell.mechanics.mechanics.vel"].apply(lambda x: np.array(x, dtype=float))
+        df["element.cell.mechanics.mechanics.random_vector"] = df["element.cell.mechanics.mechanics.random_vector"].apply(lambda x: np.array(x))
+        return df
+    else:
+        raise ValueError(f"Could not find iteration {iteration} in saved results")
+
+def get_all_iterations(output_path):
+    return sorted([int(x) for x in os.listdir(Path(output_path) / "cell_storage/json")])
+
+def __iter_to_cells(iteration_dir):
+    iteration, dir = iteration_dir
+    return (int(iteration), _combine_batches(dir / iteration))
+
+def get_particles_at_all_iterations(output_path, threads=1):
+    dir = Path(output_path) / "cell_storage/json/"
+    runs = [(x, dir) for x in os.listdir(dir)]
+    pool = mp.Pool(threads)
+    result = list(pool.map(__iter_to_cells, runs[:10]))
+    return result
--- cellular_raza-examples/autophagy_pyo3/run_sim.py
+++ cellular_raza-examples/autophagy_pyo3/run_sim.py
@@ -3,43 +3,27 @@ from cr_autophagy_pyo3 import SimulationSettings, run_simulation
 simulation_settings = SimulationSettings()
 
 # Settings Cargo
-simulation_settings.n_cells_cargo=3
-simulation_settings.particle_template_cargo.interaction.bound = 0.02
-simulation_settings.particle_template_cargo.interaction.well_depth = 0.01
-simulation_settings.particle_template_cargo.interaction.potential_width = 1/10
-simulation_settings.particle_template_cargo.interaction.cell_radius = 1.0
-simulation_settings.particle_template_cargo.interaction.cutoff = simulation_settings.particle_template_cargo.interaction.cell_radius + simulation_settings.particle_template_cargo.interaction.potential_width
-simulation_settings.particle_template_cargo.interaction.avidity = 1.0
-
-simulation_settings.particle_template_cargo.mechanics.damping = 1.5
-simulation_settings.particle_template_cargo.mechanics.mass = 4/3*3.1415
-simulation_settings.particle_template_cargo.mechanics.kb_temperature = 0.00001
+simulation_settings.n_cells_cargo=300
+# simulation_settings.particle_template_cargo.interaction.potential_strength = 0.01
 
 # Settings Atg11/Receptor
-simulation_settings.n_cells_atg11_receptor=1
-simulation_settings.particle_template_atg11_receptor.interaction.bound = 0.02
-simulation_settings.particle_template_atg11_receptor.interaction.well_depth = 0.01
-simulation_settings.particle_template_atg11_receptor.interaction.potential_width = 0.5/10
-simulation_settings.particle_template_atg11_receptor.interaction.cell_radius = 0.5
-simulation_settings.particle_template_atg11_receptor.interaction.cutoff = simulation_settings.particle_template_atg11_receptor.interaction.cell_radius + simulation_settings.particle_template_atg11_receptor.interaction.potential_width
-simulation_settings.particle_template_atg11_receptor.interaction.avidity = 1.0
+simulation_settings.n_cells_atg11_receptor=1500
+# simulation_settings.particle_template_atg11_receptor.interaction.potential_strength = 0.01
+# simulation_settings.particle_template_atg11_receptor.mechanics.kb_temperature = 0.01
 
-simulation_settings.particle_template_atg11_receptor.mechanics.damping = 0.5
-simulation_settings.particle_template_atg11_receptor.mechanics.mass = 4/3*3.1415*0.5**3
-simulation_settings.particle_template_atg11_receptor.mechanics.kb_temperature = 0.0075
+# Time
+simulation_settings.n_threads = 6
+simulation_settings.dt = 1.0
+simulation_settings.n_times = 50_001
+simulation_settings.save_interval = 50
 
-# General Settings
-simulation_settings.n_threads = 1
-simulation_settings.dt = 0.5
-simulation_settings.domain_size = 20
-simulation_settings.domain_n_voxels = 5
-simulation_settings.n_times = 10_001
-simulation_settings.save_interval = 20
+# Domain
+simulation_settings.domain_size = 60
+simulation_settings.domain_size_cargo = 16
+simulation_settings.domain_n_voxels = 7
+
+# Other settings
 simulation_settings.show_progressbar = True
 
 if __name__ == "__main__":
     output_path = run_simulation(simulation_settings)
-else:
-    import os
-    from pathlib import Path
-    output_path = Path("out/autophagy/") / os.listdir("out/autophagy/")[-1]
