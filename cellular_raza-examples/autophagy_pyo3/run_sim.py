1946c9bdb653d8cd2e3b1b2122f11f540fe8f3bd
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-11-23 02:41:55 +0100

    autophagy: parameters and settings that produce some results

--- cellular_raza-examples/autophagy_pyo3/run_sim.py
+++ cellular_raza-examples/autophagy_pyo3/run_sim.py
@@ -3,24 +3,24 @@ from cr_autophagy_pyo3 import SimulationSettings, run_simulation
 simulation_settings = SimulationSettings()
 
 # Settings Cargo
-simulation_settings.n_cells_cargo=300
-# simulation_settings.particle_template_cargo.interaction.potential_strength = 0.01
+simulation_settings.n_cells_cargo=100
+simulation_settings.particle_template_cargo.interaction.avidity = 1.0
 
 # Settings Atg11/Receptor
-simulation_settings.n_cells_atg11_receptor=1500
-# simulation_settings.particle_template_atg11_receptor.interaction.potential_strength = 0.01
-# simulation_settings.particle_template_atg11_receptor.mechanics.kb_temperature = 0.01
+simulation_settings.n_cells_atg11_receptor=700
+simulation_settings.particle_template_atg11_receptor.interaction.avidity = 1.0
+simulation_settings.particle_template_atg11_receptor.mechanics.kb_temperature = 0.0025
 
 # Time
-simulation_settings.n_threads = 6
-simulation_settings.dt = 1.0
-simulation_settings.n_times = 50_001
-simulation_settings.save_interval = 50
+simulation_settings.n_threads = 10
+simulation_settings.dt = 2.0
+simulation_settings.n_times = 30_001
+simulation_settings.save_interval = 1000
 
 # Domain
-simulation_settings.domain_size = 60
-simulation_settings.domain_size_cargo = 16
-simulation_settings.domain_n_voxels = 7
+simulation_settings.domain_size = 40
+simulation_settings.domain_size_cargo = 11
+simulation_settings.domain_n_voxels = 5
 
 # Other settings
 simulation_settings.show_progressbar = True
