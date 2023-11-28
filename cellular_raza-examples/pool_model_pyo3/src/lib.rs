6a0df07b1060b28895f2b69c41e3d96af3853cc6
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-11-28 20:13:34 +0100

    remove old run_simulation function and also add Langevin2D to python classes

--- cellular_raza-examples/pool_model_pyo3/src/lib.rs
+++ cellular_raza-examples/pool_model_pyo3/src/lib.rs
@@ -4,16 +4,7 @@ mod simulation;
 use bacteria_properties::*;
 use simulation::*;
 
-use pyo3::{exceptions::PyValueError, prelude::*};
-
-/// Python version function of [run_simulation](simulation::run_simulation)
-#[pyfunction]
-fn run_simulation(simulation_settings: SimulationSettings) -> Result<std::path::PathBuf, PyErr> {
-    match run_simulation_rs(simulation_settings) {
-        Ok(b) => Ok(b),
-        Err(e) => Err(PyValueError::new_err(format!("{:?}", e))),
-    }
-}
+use pyo3::prelude::*;
 
 #[pymodule]
 fn cr_pool_model_pyo3(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
@@ -22,7 +13,7 @@ fn cr_pool_model_pyo3(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<SimulationSettings>()?;
     m.add_class::<Bacteria>()?;
     m.add_class::<BacteriaInteraction>()?;
-    m.add_class::<BacteriaMechanicsModel2D>()?;
+    m.add_class::<cellular_raza::building_blocks::cell_building_blocks::mechanics::Langevin2D>()?;
     m.add_class::<BacteriaReactions>()?;
 
     Ok(())
