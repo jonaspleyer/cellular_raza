98047f5bc43203c2612787982732b07304a293a5
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-12-01 05:42:43 +0100

    adapt parameters to new conditions and more towards what polina has supplied

--- cellular_raza-examples/pool_model_pyo3/run_sim.py
+++ cellular_raza-examples/pool_model_pyo3/run_sim.py
@@ -9,22 +9,21 @@ simulation_settings.domain.initial_concentrations = [2.0, 0]
 simulation_settings.domain.diffusion_constants = [1.0, 0]
 
 # Bacteria - General
-simulation_settings.n_bacteria_initial = 1
+simulation_settings.n_bacteria_initial_1 = 5
+simulation_settings.n_bacteria_initial_2 = 5
 
 # Bacteria - Interaction & Mechanics
-simulation_settings.bacteria_mechanics.kb_temperature = 0.01
 simulation_settings.bacteria_interaction.potential_strength = 0.5
 
 # Bacteria - Reactions
-simulation_settings.bacteria_reactions.uptake_rates = [0.01, 0.0]
+simulation_settings.bacteria_reactions.uptake_rate = 0.01
 
 # Bacteria - Cycle
-simulation_settings.bacteria_cycle.lag_phase_transition_rate = 0.0001
 simulation_settings.bacteria_cycle.food_to_volume_conversion = 1e-5
 
 # General Settings
 simulation_settings.n_threads = 4
-simulation_settings.dt = 0.5
+simulation_settings.dt = 0.3
 simulation_settings.n_times = 40_001
 simulation_settings.save_interval = 250
 
