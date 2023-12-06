dd650f4583706297015058efe2781c731e1b9570
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-12-06 17:40:43 +0100

    use new code in python script to run full simulation

--- cellular_raza-examples/pool_model_pyo3/run_sim.py
+++ cellular_raza-examples/pool_model_pyo3/run_sim.py
@@ -1,35 +1,27 @@
-from cr_pool_model_pyo3 import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-simulation_settings = SimulationSettings()
+import cr_pool_model_pyo3 as crp_py
 
-# Domain
-simulation_settings.domain.size = 250
+# simulation_settings = SimulationSettings()
 
-simulation_settings.domain.initial_concentrations = [2.0, 0]
-simulation_settings.domain.diffusion_constants = [1.0, 0]
+domain = crp_py.Domain()
+meta_params = crp_py.MetaParams()
 
-# Bacteria - General
-simulation_settings.n_bacteria_initial_1 = 5
-simulation_settings.n_bacteria_initial_2 = 5
+n_cells_1 = 5
+n_cells_2 = 5
 
-# Bacteria - Interaction & Mechanics
-simulation_settings.bacteria_interaction.potential_strength = 0.5
+cells = []
+for i in range(n_cells_1 + n_cells_2):
+    cell = crp_py.BacteriaTemplate()
+    cell.mechanics.pos = np.random.uniform(0.0, domain.size, [2])
+    if i > n_cells_1:
+        cell.cellular_reactions.species = crp_py.Species.S1
 
-# Bacteria - Reactions
-simulation_settings.bacteria_reactions.uptake_rate = 0.01
+    cells.append(cell)
 
-# Bacteria - Cycle
-simulation_settings.bacteria_cycle.food_to_volume_conversion = 1e-5
-
-# General Settings
-simulation_settings.n_threads = 4
-simulation_settings.dt = 0.3
-simulation_settings.n_times = 40_001
-simulation_settings.save_interval = 250
-
-if __name__ == "__main__":
-    output_path = run_simulation(simulation_settings)
-else:
-    import os
-    from pathlib import Path
-    output_path = Path("out/pool_model/") / sorted(os.listdir("out/pool_model/"))[-1]
+output_path = crp_py.run_simulation(
+    cells,
+    domain,
+    meta_params,
+)
