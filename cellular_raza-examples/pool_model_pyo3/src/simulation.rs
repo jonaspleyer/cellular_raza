e7524f4730fb0e6e91c0be5f5ae09ce6ca971caf
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-12-06 17:40:10 +0100

    adapt run_simulation method to new interface

--- cellular_raza-examples/pool_model_pyo3/src/simulation.rs
+++ cellular_raza-examples/pool_model_pyo3/src/simulation.rs
@@ -117,74 +117,122 @@ impl MetaParams {
     }
 }
 
-fn save_simulation_settings(
+fn save_initial_state(
     path: &std::path::PathBuf,
-    simulation_settings: &SimulationSettings,
+    cells: &Vec<BacteriaTemplate>,
+    domain: &Domain,
+    meta_params: &MetaParams,
 ) -> PyResult<()> {
     // Also save the SimulationSettings into the same folder
-    let mut save_path = path.clone();
-    save_path.push("simulation_settings.json");
-    let f = std::fs::File::create(save_path)?;
-    let writer = std::io::BufWriter::new(f);
-    serde_json::to_writer_pretty(writer, &simulation_settings).or_else(|e| {
+    let mut save_path_cells = path.clone();
+    let mut save_path_domain = path.clone();
+    let mut save_path_meta_params = path.clone();
+    save_path_cells.push("initial_cells.json");
+    save_path_domain.push("domain.json");
+    save_path_meta_params.push("meta_params.json");
+
+    let f_cells = std::fs::File::create(save_path_cells)?;
+    let f_domain = std::fs::File::create(save_path_domain)?;
+    let f_meta_params = std::fs::File::create(save_path_meta_params)?;
+
+    let writer_cells = std::io::BufWriter::new(f_cells);
+    let writer_domain = std::io::BufWriter::new(f_domain);
+    let writer_meta_params = std::io::BufWriter::new(f_meta_params);
+
+    serde_json::to_writer_pretty(writer_cells, &cells).or_else(|e| {
         Err(pyo3::PyErr::new::<pyo3::exceptions::PyValueError, _>(
             format!("serde_json error in writing simulation settings to file: {e}"),
         ))
     })?;
+    serde_json::to_writer_pretty(writer_domain, &domain).or_else(|e| {
+        Err(pyo3::PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            format!("serde_json error in writing simulation settings to file: {e}"),
+        ))
+    })?;
+    serde_json::to_writer_pretty(writer_meta_params, &meta_params).or_else(|e| {
+        Err(pyo3::PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            format!("serde_json error in writing simulation settings to file: {e}"),
+        ))
+    })?;
+
     Ok(())
 }
 
 #[pyfunction]
-pub fn run_simulation(
-    simulation_settings: SimulationSettings,
+pub fn generate_cells(
     py: Python,
-) -> PyResult<std::path::PathBuf> {
-    // Fix random seed
-    let mut rng = ChaCha8Rng::seed_from_u64(simulation_settings.random_seed);
-
+    n_bacteria_1: usize,
+    n_bacteria_2: usize,
+    domain: Domain,
+    meta_params: MetaParams,
+    bacteria_template: Option<BacteriaTemplate>,
+) -> PyResult<Vec<BacteriaTemplate>> {
     // ###################################### DEFINE CELLS IN SIMULATION ######################################
-    let mechanics: NewtonDamped2D = simulation_settings.bacteria_mechanics.extract(py)?;
-    let cycle: BacteriaCycle = simulation_settings.bacteria_cycle.extract(py)?;
-    let interaction: BacteriaInteraction = simulation_settings.bacteria_interaction.extract(py)?;
-    let cellular_reactions: BacteriaReactions =
-        simulation_settings.bacteria_reactions.extract(py)?;
-    let domain_setup: Domain = simulation_settings.domain.extract(py)?;
 
-    let cells = (0..simulation_settings.n_bacteria_initial_1+simulation_settings.n_bacteria_initial_2)
+    let mut rng = ChaCha8Rng::seed_from_u64(meta_params.random_seed);
+    let bacteria = match bacteria_template {
+        Some(template) => template.clone(),
+        None => BacteriaTemplate::default(py)?,
+    };
+
+    let cells = (0..n_bacteria_1 + n_bacteria_2)
         .map(|n_cell| {
-            let x = rng.gen_range(0.0..domain_setup.size);
-            let y = rng.gen_range(0.0..domain_setup.size);
+            let mut new_bacteria = bacteria.clone();
+            let x = rng.gen_range(0.0..domain.size);
+            let y = rng.gen_range(0.0..domain.size);
 
             // Set new position of bacteria
             let pos = Vector2::from([x, y]);
-            let mut mechanics_new = mechanics.clone();
-            let mut cellular_reactions_new = cellular_reactions.clone();
-            if n_cell >= simulation_settings.n_bacteria_initial_1 {
-                cellular_reactions_new.species = Species::S2;
+
+            if n_cell >= n_bacteria_1 {
+                let mut reactions = new_bacteria
+                    .cellular_reactions
+                    .extract::<BacteriaReactions>(py)?;
+                reactions.species = Species::S2;
+                new_bacteria.cellular_reactions = Py::new(py, reactions)?;
             }
-            mechanics_new.set_pos(&pos);
-            Ok(Bacteria {
-                mechanics: mechanics_new,
-                cycle: cycle.clone(),
-                interaction: interaction.clone(),
-                cellular_reactions: cellular_reactions_new,
-                interactionextracellulargradient: GradientSensing,
-            })
+            let mut mechanics = new_bacteria.mechanics.extract::<NewtonDamped2D>(py)?;
+            mechanics.set_pos(&pos);
+            new_bacteria.mechanics = Py::new(py, mechanics)?;
+            Ok(new_bacteria)
         })
         .collect::<PyResult<Vec<_>>>()?;
+    Ok(cells)
+}
 
+#[pyfunction]
+pub fn run_simulation(
+    py: Python,
+    cells: Vec<BacteriaTemplate>,
+    domain: Domain,
+    meta_params: MetaParams,
+) -> PyResult<std::path::PathBuf> {
     // ###################################### CREATE SUPERVISOR AND RUN SIMULATION ######################################
-    let domain = match domain_setup.n_voxels {
+    let simulation_domain = match domain.n_voxels {
         Some(n_voxels) => CartesianCuboid2::from_boundaries_and_n_voxels(
             [0.0; 2],
-            [domain_setup.size; 2],
+            [domain.size; 2],
             [n_voxels; 2],
         ),
-        None => CartesianCuboid2::from_boundaries_and_interaction_ranges(
-            [0.0; 2],
-            [domain_setup.size; 2],
-            [2.0 * interaction.cell_radius; 2],
-        ),
+        None => {
+            let mut max_cell_radius = cells
+                .iter()
+                .map(|cell| {
+                    cell.interaction
+                        .extract::<BacteriaInteraction>(py)
+                        .unwrap()
+                        .cell_radius
+                })
+                .fold(f64::MIN, f64::max);
+            if max_cell_radius <= 0.0 {
+                max_cell_radius = domain.size / 3.0;
+            }
+            CartesianCuboid2::from_boundaries_and_interaction_ranges(
+                [0.0; 2],
+                [domain.size; 2],
+                [2.0 * max_cell_radius; 2],
+            )
+        }
     }
     .or_else(|e| {
         Err(pyo3::PyErr::new::<pyo3::exceptions::PyValueError, _>(
@@ -192,43 +240,50 @@ pub fn run_simulation(
         ))
     })?;
 
+    // TODO try to avoid this cloning right here!
+    let simulation_cells = cells
+        .clone()
+        .into_iter()
+        .map(|cell_template| Bacteria::from(py, cell_template))
+        .collect::<PyResult<Vec<_>>>()?;
+
     let time = TimeSetup {
         t_start: 0.0,
-        t_eval: (0..simulation_settings.n_times)
+        t_eval: (0..meta_params.n_times)
             .map(|n| {
                 (
-                    n as f64 * simulation_settings.dt,
-                    n % simulation_settings.save_interval == 0,
+                    n as f64 * meta_params.dt,
+                    n % meta_params.save_interval == 0,
                 )
             })
             .collect(),
     };
 
-    let meta_params = SimulationMetaParams {
-        n_threads: simulation_settings.n_threads,
-        rng_seed: simulation_settings.random_seed,
+    let simulation_meta_params = SimulationMetaParams {
+        n_threads: meta_params.n_threads,
+        rng_seed: meta_params.random_seed,
     };
 
-    let storage = StorageConfig::from_path(std::path::Path::new(&simulation_settings.save_path))
-        .add_date(simulation_settings.save_add_date);
+    let storage = StorageConfig::from_path(std::path::Path::new(&meta_params.save_path))
+        .add_date(meta_params.save_add_date);
     // storage.export_formats = vec![ExportOptions::Vtk];
 
     let simulation_setup = create_simulation_setup!(
-        Domain: domain,
-        Cells: cells,
+        Domain: simulation_domain,
+        Cells: simulation_cells,
         Time: time,
-        MetaParams: meta_params,
+        MetaParams: simulation_meta_params,
         Storage: storage
     );
 
-    let voxel_definition_strategy =
-        |voxel: &mut CartesianCuboidVoxel2<NUMBER_OF_REACTION_COMPONENTS>| {
-            voxel.diffusion_constant = ReactionVector::from([domain_setup.diffusion_constants]);
-            voxel.extracellular_concentrations =
-                ReactionVector::from([domain_setup.initial_concentrations]);
-            voxel.degradation_rate = ReactionVector::zero();
-            voxel.production_rate = ReactionVector::zero();
-        };
+    let voxel_definition_strategy = |voxel: &mut CartesianCuboidVoxel2<
+        NUMBER_OF_REACTION_COMPONENTS,
+    >| {
+        voxel.diffusion_constant = ReactionVector::from([domain.diffusion_constants]);
+        voxel.extracellular_concentrations = ReactionVector::from([domain.initial_concentrations]);
+        voxel.degradation_rate = ReactionVector::zero();
+        voxel.production_rate = ReactionVector::zero();
+    };
 
     let strategies = Strategies {
         voxel_definition_strategies: &voxel_definition_strategy,
@@ -237,9 +292,14 @@ pub fn run_simulation(
     // let simulation_result = run_full_simulation!(setup, settings, [Mechanics, Interaction, Cycle])?;
     let mut supervisor =
         SimulationSupervisor::initialize_with_strategies(simulation_setup, strategies);
-    supervisor.config.show_progressbar = simulation_settings.show_progressbar;
+    supervisor.config.show_progressbar = meta_params.show_progressbar;
 
-    save_simulation_settings(&supervisor.storage.get_location(), &simulation_settings)?;
+    save_initial_state(
+        &supervisor.storage.get_location(),
+        &cells,
+        &domain,
+        &meta_params,
+    )?;
 
     let simulation_result = supervisor.run_full_sim().unwrap();
 
