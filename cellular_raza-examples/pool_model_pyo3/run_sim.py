0332ba18b9b067f090073f08285e372b14d8caf4
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-12-08 03:30:14 +0100

    new functions to evenly distribute cells in the simulation domain

--- cellular_raza-examples/pool_model_pyo3/run_sim.py
+++ cellular_raza-examples/pool_model_pyo3/run_sim.py
@@ -4,9 +4,83 @@ import scipy as sp
 import cr_pool_model_pyo3 as crp_py
 
 
+def calculate_lattice_points(x_min, x_max, n_agents):
+    # Calculate the lowest number possible such that
+    # we can create a almost even lattice based on that
+    m = np.ceil(np.sqrt(n_agents)) + 1
+    dx = (x_max - x_min) / m
+
+    x, y = np.mgrid[x_min+dx:x_max:dx, x_min+dx:x_max:dx]
+    if m % 2 == 0:
+        z = np.vstack([
+            x.reshape(-1),
+            y.reshape(-1)]
+        ).T
+    else:
+        for i in range(len(y)):
+            y[i] = np.roll(y[i], -(i % 2))
+        z = np.vstack([
+            x.reshape(-1),
+            y.reshape(-1)
+        ]).T
+    return z
 
 
+def calculate_index_distributions(n_cells_1, n_cells_2, n_positions):
+    indices = np.arange(n_positions)
 
+    ind_even = indices[::2]
+    ind_uneven = indices[1::2]
+
+    ind1_even = ind_even[:n_cells_1]
+    ind1_uneven = ind_uneven[:n_cells_1-len(ind1_even)]
+
+    ind1 = np.sort(np.hstack([ind1_even, ind1_uneven]))
+    ind2 = np.sort(np.setdiff1d(indices, ind1))[:n_cells_2]
+
+    return ind1, ind2
+
+
+def generate_cells(n_cells_1, n_cells_2, domain, uniformity=1.0, randomness=1.0, pad=0.15, seed=0):
+    """
+    n_cells_1: int
+    n_cells_2: int
+    uniformity: float
+        Floating point number between 0.0 and 1.0
+    """
+    # Fix numpy random seed
+    rng = np.random.default_rng(seed)
+
+    # Get the domain size
+    d_min =    pad *domain.size
+    d_max = (1-pad)*domain.size
+    u = uniformity
+    r = randomness
+
+    positions = calculate_lattice_points(0.0, domain.size, n_cells_1 + n_cells_2)
+    ind1, ind2 = calculate_index_distributions(n_cells_1, n_cells_2, len(positions))
+
+    cells = []
+    for i in range(n_cells_1 + n_cells_2):
+        # Cell Settings
+        cell = crp_py.BacteriaTemplate()
+        cell.cycle.lag_phase_transition_rate_1 = 0.001
+        cell.cycle.lag_phase_transition_rate_1 = 0.0005
+
+        if i < n_cells_1:
+            # x = rng.uniform(d_min, (1-u)*d_min + u*d_max)
+            cell.mechanics.pos = positions[ind1[i]]
+        else:
+            # x = rng.uniform(u*d_min + (1-u)*d_max, d_max)
+            cell.cellular_reactions.species = crp_py.Species.S2
+            cell.mechanics.pos = positions[ind2[i - n_cells_1]]
+
+        # y = rng.uniform(d_min, d_max)
+        # cell.mechanics.pos = [x, y]
+        # cell.mechanics.pos = positions[i]
+
+        cells.append(cell)
+    return cells
 
 
 if __name__ == "__main__":
