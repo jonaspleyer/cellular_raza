8ea7dd53c22a4c145588b1358126251be1fd30ba
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-12-08 03:29:48 +0100

    use new interface to create cells and place in domain

--- cellular_raza-examples/pool_model_pyo3/run_sim.py
+++ cellular_raza-examples/pool_model_pyo3/run_sim.py
@@ -1,27 +1,30 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import scipy as sp
 
 import cr_pool_model_pyo3 as crp_py
 
-# simulation_settings = SimulationSettings()
 
-domain = crp_py.Domain()
-meta_params = crp_py.MetaParams()
 
-n_cells_1 = 5
-n_cells_2 = 5
 
-cells = []
-for i in range(n_cells_1 + n_cells_2):
-    cell = crp_py.BacteriaTemplate()
-    cell.mechanics.pos = np.random.uniform(0.0, domain.size, [2])
-    if i > n_cells_1:
-        cell.cellular_reactions.species = crp_py.Species.S1
 
-    cells.append(cell)
 
-output_path = crp_py.run_simulation(
-    cells,
-    domain,
-    meta_params,
-)
+
+if __name__ == "__main__":
+    # Domain Settings
+    domain = crp_py.Domain()
+    domain.size = 1_000
+    domain.diffusion_constants = [15.0, 5.0]
+
+    # Meta Parameters
+    meta_params = crp_py.MetaParams()
+    meta_params.save_interval = 500
+    meta_params.n_times = 20_001
+    meta_params.n_threads = 8
+
+    cells = generate_cells(18, 18, domain)
+
+    output_path = crp_py.run_simulation(
+        cells,
+        domain,
+        meta_params,
+    )
