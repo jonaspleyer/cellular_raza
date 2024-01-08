29bd814ed2ab45f83d7a67d12b6f7a0feb10f056
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-01-08 04:09:01 +0100

    specify features supported by backend

--- cellular_raza-core/src/backend/mod.rs
+++ cellular_raza-core/src/backend/mod.rs
@@ -5,6 +5,22 @@
 ///
 /// The user can manage the simulation flow by means of individual funtions or by creating a
 /// [SimulationSupervisor](cpu_os_threads::supervisor::SimulationSupervisor).
+///
+/// # Supported Cellular Aspects
+/// | Aspect | Support | Comment |
+/// | --- |:---:| --- |
+/// | [Cycle](cellular_raza_concepts::cycle) | ✅ | Fully generic except `Float=f64` |
+/// | [Mechanics](cellular_raza_concepts::mechanics) | ✅ | Fully generic except `Float=f64` |
+/// | [Interaction](cellular_raza_concepts::interaction) | ✅ | Fully generic |
+/// | [Reactions](cellular_raza_concepts::interaction) | ✅ | Fully generic |
+/// | [Volume](cellular_raza_concepts::interaction) | ✅ | |
+///
+/// # Other Features
+/// | Aspect | Support | Comment |
+/// | --- |:---:| --- |
+/// | [Domain](cellular_raza_concepts::domain::Domain) | ✅ | |
+/// | [DomainNew](cellular_raza_concepts::domain_new::Domain) | ❌ | |
+/// | [Plotting](cellular_raza_concepts::plotting) | ✅ |
 pub mod cpu_os_threads;
 
 /* pub trait Backend {
