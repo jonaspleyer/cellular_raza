6f95d3a073b68062ad59f98288bbf54c6cb70a18
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-01-17 16:55:46 +0100

    add description of crate and proc_macro module

--- cellular_raza-core/src/lib.rs
+++ cellular_raza-core/src/lib.rs
@@ -1,10 +1,37 @@
 // TODO make this be deny instead of warning
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
+//! This crate collects objects and methods needed to run a numerical simulation of
+//! given objects that satisfy the given [concepts].
+//!
+//! ## Backends
+//! This crate supports multiple types of backends.
+//! Currently, the [backend::cpu_os_threads] backend is the general-purpose solver which
+//! can deal with (almost) all simulation [concepts].
+//! In the future, the [backend::chili] backend will be replacing it, delivering
+//! better performance, modularity while also updating [concepts].
+//!
+//! ## Storage
+//! We distinguish between a full (de-)serialization of the simulation
+//! and exporting data from individual simulation steps.
+//!
+//! ### Full (de)serialization
+//! The first approach allows for a full reload of the total simulation which in principle
+//! enables methods such as starting/stopping the simulation and continuing from the last
+//! known point.
+//! This can also be used to avoid numerical solving problems by restarting from the
+//! last known good save point.
+//! However, the latter functionalities do not exist currently but are planned for future releases.
+//!
+//! ### Exporting
+//! This approach allows to take cells or domain objects and extract information to then
+//! save these in a given format.
+//! The methods needed to do this have not yet been developed and are part of future releases.
 
 /// The backend controls the simulation flow. Multiple variants could be available in the future.
 pub mod backend;
 
+/// Derive traits and generate code for different traits.
 pub mod proc_macro;
 
 /// Interface and methods to store and load simulation aspects.
