37097ee834854bdfd2a96c843224a4e12bd92c1c
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-01-23 05:09:41 +0100

    flatten hirarchy for exposure of backend functionality

--- cellular_raza-core/src/backend/chili/mod.rs
+++ cellular_raza-core/src/backend/chili/mod.rs
@@ -12,16 +12,16 @@ pub struct SubDomainPlainIndex(usize);
 pub struct CellIdentifier(VoxelPlainIndex, u64);
 
 /// Contains structs to store aspects of the simulation and macros to construct them.
-pub mod aux_storage;
+mod aux_storage;
+mod datastructures;
+mod errors;
+mod setup;
+mod simulation_flow;
+mod solvers;
 
-pub mod errors;
-
-pub mod datastructures;
-
-/// Specify a way to set-up and start the simulation.
-pub mod setup;
-
-pub mod solvers;
-
-/// Numerical integration and solving of the model.
-pub mod simulation_flow;
+pub use aux_storage::*;
+pub use datastructures::*;
+pub use errors::*;
+pub use setup::*;
+pub use simulation_flow::*;
+pub use solvers::*;
