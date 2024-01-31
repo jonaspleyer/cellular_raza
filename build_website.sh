fb9a3b286687c7bcb65b3d931e3f6783c966f140
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-01-31 16:37:12 +0100

    script to automatically generate documentation and put it into the content folder of the homepage

---
+++ build_website.sh
@@ -0,0 +1,28 @@
+TARGETS=(
+	cellular_raza
+	cellular_raza-benchmarks
+	cellular_raza-book
+	cellular_raza-building-blocks
+	cellular_raza-concepts
+	cellular_raza-concepts-derive
+	cellular_raza-core
+	cellular_raza-core-proc-macro
+)
+
+cargo clean --doc
+
+for target in ${TARGETS[@]}; do
+	cd $target
+	cargo cr_doc
+	cd ..
+done
+
+cp -r target/doc cellular_raza-homepage/cr_doc
+cd cellular_raza-homepage
+hugo
+	rm -rf content/docs
+mv cr_doc content/docs
+cd ..
+
+echo "Finished building website"
+
