bdd5eb3036053b15311137e846ed5b5b04a04b0c
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-02-02 22:33:55 +0100

    transform to build script with individual options for partial building

--- build_website.sh
+++ build_website.sh
@@ -1,37 +1,92 @@
-TARGETS=(
-	cellular_raza
-	cellular_raza-benchmarks
-	cellular_raza-building-blocks
-	cellular_raza-concepts
-	cellular_raza-concepts-derive
-	cellular_raza-core
-	cellular_raza-core-proc-macro
-)
+generate_docs() {
+    TARGETS=(
+        cellular_raza
+        cellular_raza-benchmarks
+        cellular_raza-building-blocks
+        cellular_raza-concepts
+        cellular_raza-concepts-derive
+        cellular_raza-core
+        cellular_raza-core-proc-macro
+    )
 
-cargo clean --doc
+    cargo clean --doc
 
-for target in ${TARGETS[@]}; do
-	cd $target
-	cargo cr_doc
-	cd ..
-done
+    for target in ${TARGETS[@]}; do
+        cd $target
+        cargo +nightly rustdoc --all-features -- --cfg doc_cfg
+        cd ..
+    done
+}
 
-cp -r target/doc cellular_raza-homepage/cr_doc
-cd cellular_raza-homepage
+build_website() {
+    # Copy generated code to website folder
+    cp -r target/doc cellular_raza-homepage/cr_doc
 
-rm -rf content/docs
-mv cr_doc content/docs
+    # Swap old for new folder
+        cd cellular_raza-homepage
+    rm -rf content/docs
+    mv cr_doc content/docs
 
-hugo -d public_html
+    hugo -d public_html
+    cd ..
+}
 
-# This command can also be used to simply recursively copy all files
-# id does not filter duplicates and thus takes a long time
-# scp -r public_html celluld@www139.your-server.de:/
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo " -h --help        Display this help message"
+    echo " -d --doc         Generate rust documentation"
+    echo " -w --website     Build website"
+    echo " -u --upload      Upload to server"
+    echo " -a --all         Do a full build with upload. Same as -d -w -u"
+}
 
-cwd="$PWD"
+upload() {
+    cd cellular_raza-homepage
+    cwd="$PWD"
+    # This command can also be used to simply recursively copy all files
+    # id does not filter duplicates and thus takes a long time
+    # scp -r public_html celluld@www139.your-server.de:/
 
-sftp celluld@www139.your-server.de <<EOF
-put -a $cwd/public_html
-exit
+    sftp celluld@www139.your-server.de<<EOF
+    put -R $cwd/public_html
+    exit
 EOF
+    cd ..
+}
+
+handle_options() {
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -d | --doc)
+                generate_docs
+                exit 0
+                ;;
+            -w | --website)
+                build_website
+                exit 0
+                ;;
+            -u | --upload)
+                upload
+                exit 0
+                ;;
+            -a | --all)
+                generate_docs
+                build_website
+                upload
+                exit 0
+                ;;
+            -h | --help)
+                usage
+                exit 0
+                ;;
+            \?)
+                usage
+                exit 1
+                ;;
+        esac
+    done
+}
+
+handle_options "$@"
 
