ebb4790d1e53c1817ae218da7e61803503914fd3
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-02-02 02:02:14 +0100

    add properties from the pool model as a whole cellular model

--- cellular_raza-building-blocks/src/cell_models/mod.rs
+++ cellular_raza-building-blocks/src/cell_models/mod.rs
@@ -5,3 +5,6 @@
 /// [concepts](crate::concepts). The concepts are afterwards derived automatically for the
 /// [ModularCell](modular_cell::ModularCell) struct.
 pub mod modular_cell;
+
+#[cfg(feature = "pyo3")]
+pub mod pool_bacteria;
