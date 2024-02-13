d927362653a0995e0d494f2e8da6a62703327934
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-02-13 05:47:06 +0100

    add empty macro for plan with how to construct it in the future

---
+++ cellular_raza-core/src/backend/chili/run_sim.rs
@@ -0,0 +1,22 @@
+/// We do run simulations sometimes
+///
+/// General idea:
+/// - Definitions
+///     - Define objects (such as aux storage)
+/// - Implement settings
+///     - Build Storage Manager
+///     - Build Time-Stepper
+///     - (tracing handler?)
+/// - Build thunk (thin function) which does single update step
+/// - Call function parallelized
+/// - Combine threads
+///     - Return SimulationResult with Storage Manager
+///     
+#[macro_export]
+#[doc(hidden)]
+macro_rules! run_simulation {
+    () => {}
+}
+
+#[doc(inline)]
+pub use crate::run_simulation;
