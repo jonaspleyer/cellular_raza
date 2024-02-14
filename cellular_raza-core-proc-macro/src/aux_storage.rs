e214e974e14f51358f7cf4fa858c017dd06117bd
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-02-14 01:50:18 +0100

    remove link to main core crate

--- cellular_raza-core-proc-macro/src/aux_storage.rs
+++ cellular_raza-core-proc-macro/src/aux_storage.rs
@@ -1,5 +1,5 @@
 //! The main two macros are [derive_aux_storage] and [construct_aux_storage] which allow to
-//! derive individual aspects of the the update traits in [cellular_raza_core::backend::chili::aux_storage].
+//! derive individual aspects of the the update traits in `cellular_raza_core`
 
 use proc_macro::TokenStream;
 use quote::quote;
