2e3d9dc8908a7037c9550de07c5390eb828e7c30
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-02-26 05:11:44 +0100

    test out some macros to generate main update function of backend

--- cellular_raza-examples/template/src/main.rs
+++ cellular_raza-examples/template/src/main.rs
@@ -67,6 +67,70 @@ build_communicator!(
     core_path: cellular_raza::core
 );
 
+macro_rules! gen_step_1(
+    ($sbox:ident, Mechanics) => {$sbox.update_mechanics_step_1()?;};
+    ($sbox:ident, $asp:ident) => {};
+    ($sbox:ident, $asp1:ident, $($asp:ident),*) => {
+        gen_step_1!($sbox, $asp1);
+        gen_step_1!($sbox, $($asp),*);
+    };
+);
+
+macro_rules! gen_step_2(
+    ($sbox:ident, Mechanics) => {$sbox.update_mechanics_step_2()?;};
+    ($sbox:ident, $asp:ident) => {};
+    ($sbox:ident, $asp1:ident, $($asp:ident),*) => {
+        gen_step_2!($sbox, $asp1);
+        gen_step_2!($sbox, $($asp),*);
+    };
+);
+
+macro_rules! gen_step_3(
+    ($sbox:ident, $next_time_point:ident, Mechanics) => {$sbox.update_mechanics_step_3(&$next_time_point.increment)?;};
+    ($sbox:ident, $next_time_point:ident, $asp:ident) => {};
+    ($sbox:ident, $next_time_point:ident, $asp1:ident, $($asp:ident),*) => {
+        gen_step_3!($sbox, $next_time_point, $asp1);
+        gen_step_3!($sbox, $next_time_point, $($asp),*);
+    };
+);
+
+macro_rules! gen_step_4(
+    ($sbox:ident, $storage_manager:ident, $next_time_point:ident, $pb:ident, $time_stepper:ident, Mechanics) => {
+        $sbox.sort_cells_in_voxels_step_2()?;
+           match &mut $pb {
+               Some(bar) => $time_stepper.update_bar(bar)?,
+               None => (),
+           };
+           // TODO
+           // $sbox.apply_boundary()?;
+           $sbox.save_voxels(&$storage_manager, &$next_time_point)?;
+    };
+    ($sbox:ident, $storage_manager:ident, $next_time_point:ident, $pb:ident, $time_stepper:ident, $asp:ident) => {};
+    ($sbox:ident, $storage_manager:ident, $next_time_point:ident, $pb:ident, $time_stepper:ident, $asp1:ident, $($asp:ident),*) => {
+        gen_step_4!($sbox, $storage_manager, $next_time_point, $pb, $time_stepper, $asp1);
+        gen_step_4!($sbox, $storage_manager, $next_time_point, $pb, $time_stepper, $($asp),*);
+    };
+);
+
+macro_rules! main_update(
+    (
+        subdomain: $sbox:ident,
+        storage_manager: $storage_manager:ident,
+        next_time_point: $next_time_point:ident,
+        progress_bar: $pb:ident,
+        time_stepper: $time_stepper:ident,
+        aspects: [$($asp:ident),*]
+    ) => {
+            gen_step_1!($sbox, $($asp),*);
+            $sbox.sync();
+            gen_step_2!($sbox, $($asp),*);
+            $sbox.sync();
+            gen_step_3!($sbox, $next_time_point, $($asp),*);
+            $sbox.sync();
+            gen_step_4!($sbox, $storage_manager, $next_time_point, $pb, $time_stepper, $($asp),*);
+    }
+);
+
 fn run_simulation(
     simulation_settings: SimulationSettings,
     agents: Vec<Agent>,
@@ -118,37 +182,22 @@ fn run_simulation(
 
             // Initialize the storage manager
             let storage_manager =
-                cellular_raza::prelude::StorageManager::open_or_create_with_priority(
-                    location,
-                    *key as u64,
-                    &storage_priority,
-                )?;
+            cellular_raza::prelude::StorageManager::open_or_create_with_priority(
+                location,
+                *key as u64,
+                &storage_priority,
+            )?;
 
             while let Some(next_time_point) = time_stepper.advance()? {
+                main_update!(
+                    subdomain: sbox,
+                    storage_manager: storage_manager,
+                    next_time_point: next_time_point,
+                    progress_bar: pb,
+                    time_stepper: time_stepper,
+                    aspects: [Mechanics]
+                );
                 // update_subdomain!(name: sbox, aspects: [Mechanics, Interaction]);
-                sbox.update_mechanics_step_1()?;
-
-                sbox.sync();
-
-                sbox.update_mechanics_step_2()?;
-
-                sbox.sync();
-
-                sbox.update_mechanics_step_3(&next_time_point.increment)?;
-
-                sbox.sort_cells_in_voxels_step_1()?;
-
-                sbox.sync();
-
-                sbox.sort_cells_in_voxels_step_2()?;
-                match &mut pb {
-                    Some(bar) => time_stepper.update_bar(bar)?,
-                    None => (),
-                };
-
-                sbox.apply_boundary()?;
-
-                sbox.save_voxels(&storage_manager, &next_time_point)?;
             }
             Ok(())
         })
