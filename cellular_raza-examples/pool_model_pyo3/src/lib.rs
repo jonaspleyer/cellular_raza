7b75aff37fcfeb4b869e1745d5e8d0d7c8120b8c
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-03-26 23:59:18 +0100

    migrate to new api of pyo3 0.21

--- cellular_raza-examples/pool_model_pyo3/src/lib.rs
+++ cellular_raza-examples/pool_model_pyo3/src/lib.rs
@@ -7,7 +7,7 @@ use simulation::*;
 use pyo3::prelude::*;
 
 #[pymodule]
-fn cr_pool_model_pyo3(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn cr_pool_model_pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_simulation, m)?)?;
     m.add_function(wrap_pyfunction!(generate_cells, m)?)?;
 
