289b0994358a9c7c445ec6d267dfb93f0afa24f5
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-03-28 15:04:11 +0100

    implement first iteration of derivation for base of domain

--- cellular_raza-concepts-derive/src/domain.rs
+++ cellular_raza-concepts-derive/src/domain.rs
@@ -91,16 +91,112 @@ impl syn::parse::Parse for DomainParser {
     }
 }
 
-pub struct DomainImplementer {}
+use super::cell_agent::FieldInfo;
+
+pub struct DomainImplementer {
+    name: syn::Ident,
+    generics: syn::Generics,
+    base: Option<FieldInfo>,
+    mechanics: Option<FieldInfo>,
+    reactions: Option<FieldInfo>,
+}
 
 impl From<DomainParser> for DomainImplementer {
     fn from(value: DomainParser) -> Self {
-        DomainImplementer {}
+        let mut base = None;
+        let mut mechanics = None;
+        let mut reactions = None;
+
+        value.aspects.into_iter().for_each(|aspect_field| {
+            aspect_field.aspects.into_iter().for_each(|aspect| {
+                let field_info = FieldInfo {
+                    field_type: aspect_field.field.ty.clone(),
+                    field_name: aspect_field.field.ident.clone(),
+                };
+                match aspect {
+                    DomainAspect::Mechanics => mechanics = Some(field_info),
+                    DomainAspect::Base => base = Some(field_info),
+                    DomainAspect::Reactions => reactions = Some(field_info),
+                }
+            })
+        });
+
+        DomainImplementer {
+            name: value.name,
+            generics: value.generics,
+            base,
+            mechanics,
+            reactions,
+        }
     }
 }
 
 impl DomainImplementer {
-    fn derive_domain(self) -> proc_macro2::TokenStream {
+    fn implement_base(&self) -> proc_macro2::TokenStream {
+        let struct_name = &self.name;
+        let (_, struct_ty_generics, struct_where_clause) = &self.generics.split_for_impl();
+
+        if let Some(field_info) = &self.base {
+            let field_type = &field_info.field_type;
+            let field_name = &field_info.field_name;
+            new_ident!(cell, "__cr_private_Cell");
+            new_ident!(subdomain, "__cr_private_SubDomain");
+            new_ident!(cell_iterator, "__cr_private_CellIterator");
+            let tokens = quote::quote!(#cell, #subdomain, #cell_iterator);
+
+            let where_clause = append_where_clause!(
+                struct_where_clause,
+                field_type,
+                Domain,
+                tokens
+            );
+
+            let mut generics = self.generics.clone();
+            push_ident!(generics, cell);
+            push_ident!(generics, subdomain);
+            push_ident!(generics, cell_iterator);
+            let impl_generics = generics.split_for_impl().0;
+
+            quote::quote!(
+                impl #impl_generics Domain<#tokens>
+                for #struct_name #struct_ty_generics #where_clause {
+                    type SubDomainIndex = <#field_type as Domain<#tokens>>::SubDomainIndex;
+                    type VoxelIndex = <#field_type as Domain<#tokens>>::VoxelIndex;
+
+                    fn get_all_voxel_indices(&self) -> Vec<Self::VoxelIndex> {
+                        <#field_type as Domain<#tokens>>::get_all_voxel_indices(&self.#field_name)
+                    }
+
+                    fn decompose(
+                        self,
+                        n_subdomains: core::num::NonZeroUsize,
+                        cells: #cell_iterator
+                    ) -> Result<DecomposedDomain<
+                        Self::SubDomainIndex,
+                        #subdomain,
+                        #cell
+                    >, DecomposeError>
+                    where
+                        #subdomain: SubDomain<C>
+                    {
+                        <#field_type as Domain<#tokens>>::decompose(
+                            self.#field_name,
+                            n_subdomains,
+                            cells
+                        )
+                    }
+                }
+            )
+        } else {
+            proc_macro2::TokenStream::new()
+        }
+    }
+
+    fn implement_mechanics(&self) -> proc_macro2::TokenStream {
+        todo!()
+    }
+
+    fn implement_reactions(&self) -> proc_macro2::TokenStream {
         todo!()
     }
 }
@@ -108,5 +204,10 @@ impl DomainImplementer {
 pub fn derive_domain(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let domain_parser = syn::parse_macro_input!(input as DomainParser);
     let domain_implementer: DomainImplementer = domain_parser.into();
-    domain_implementer.derive_domain().into()
+
+    let mut res = proc_macro2::TokenStream::new();
+    res.extend(domain_implementer.implement_base());
+    res.extend(domain_implementer.implement_mechanics());
+    res.extend(domain_implementer.implement_reactions());
+    super::cell_agent::wrap(res).into()
 }
