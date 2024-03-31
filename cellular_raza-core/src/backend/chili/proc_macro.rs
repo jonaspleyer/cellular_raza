fdfb2a6b6b7a70d311712af17f70d262b0c6ddb1
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-03-31 13:30:50 +0200

    move re-exports to proc_macro module

--- cellular_raza-core/src/backend/chili/proc_macro.rs
+++ cellular_raza-core/src/backend/chili/proc_macro.rs
@@ -92,3 +92,15 @@ pub use cellular_raza_core_proc_macro::Communicator;
 
 /// TODO
 pub use cellular_raza_core_proc_macro::communicator_generics_placeholders;
+
+#[doc(inline)]
+pub use cellular_raza_core_proc_macro::run_simulation;
+
+#[doc(inline)]
+pub use cellular_raza_core_proc_macro::prepare_types;
+
+#[doc(inline)]
+pub use cellular_raza_core_proc_macro::test_compatibility;
+
+#[doc(inline)]
+pub use cellular_raza_core_proc_macro::run_main;
