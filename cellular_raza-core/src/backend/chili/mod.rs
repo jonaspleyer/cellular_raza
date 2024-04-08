34f7116cc0a337f1b946718aa712599ebb20d1c3
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-08 13:41:56 +0200

    derive hash and pyclass for cellidentifier

--- cellular_raza-core/src/backend/chili/mod.rs
+++ cellular_raza-core/src/backend/chili/mod.rs
@@ -14,7 +14,8 @@ pub struct SubDomainPlainIndex(usize);
 /// This can be due to initial setup or due to other methods such as division in a cell cycle.
 /// The second parameter is a counter which is unique for each voxel.
 /// This ensures that each cell obtains a unique identifier over the course of the simulation.
-#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, Ord, PartialOrd, Serialize)]
+#[cfg_attr(feature = "pyo3", pyo3::pyclass)]
+#[derive(Clone, Copy, Debug, Deserialize, Hash, PartialEq, Eq, Ord, PartialOrd, Serialize)]
 pub struct CellIdentifier(pub VoxelPlainIndex, pub u64);
 
 /// Contains structs to store aspects of the simulation and macros to construct them.
