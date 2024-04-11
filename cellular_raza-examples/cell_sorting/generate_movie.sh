53ba355c041c519a25898fa18e2083af6f713b25
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-11 23:49:12 +0200

    script for generating plots and movie

---
+++ cellular_raza-examples/cell_sorting/generate_movie.sh
@@ -0,0 +1,13 @@
+# Generated with ffmpeg 6.1.1
+
+FOLDERS=($(ls "out/cell_sorting"))
+LAST_FOLDER=${FOLDERS[-1]}
+
+python plot.py
+
+ffmpeg \
+    -y \
+    -pattern_type glob \
+    -i "out/cell_sorting/"$LAST_FOLDER'/images/*.png' \
+    -c:v libx264 \
+    -pix_fmt yuv420p output.mp4
