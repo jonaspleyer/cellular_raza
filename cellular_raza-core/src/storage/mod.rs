976064c172584ecf3433fd794090f8557dfd51e7
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-14 13:33:55 +0200

    improve documentation of storage module

--- cellular_raza-core/src/storage/mod.rs
+++ cellular_raza-core/src/storage/mod.rs
@@ -1,3 +1,41 @@
+//! Interface and methods to store and load simulation aspects.
+//!
+//! # Overview
+//! In general, the storage solutions used can be configured with the [StorageBuilder] struct.
+//! Head there to view a list of all supported options.
+//! ```
+//! use cellular_raza_core::storage::*;
+//! let builder = StorageBuilder::new()
+//!     .priority([StorageOption::SerdeJson])
+//!     .location("/tmp")
+//!     .add_date(true)
+//!     .suffix("my_awesome_sim");
+//! ```
+//! Afterwards, we can provide this builder to our chosen backend which will take the information
+//! contained in it and then construct a [StorageManager] to actually handle loading and storing.
+//!
+//! # Storage Solutions
+//! We provide multiple storage options to choose from.
+//!
+//! ## Json
+//! Relies on the [serde_json] crate to serialize elements and store them as plain `.json` files.
+//! See [JsonStorageInterface](crate::storage::JsonStorageInterface).
+//!
+//! ## Xml
+//! Uses the [quick_xml] crate to store results as `.xml` files.
+//! See [XmlStorageInterface](crate::storage::XmlStorageInterface)
+//!
+//! ## Sled
+//! Builds an embedded database at the specified location. This database is a key-value storage and
+//! can be accessed via the [sled] crate.
+//! See [SledStorageInterface](crate::storage::SledStorageInterface)
+//!
+//! ## Sled (Temp)
+//! Identical to the previous item but will remove the database after it has dropped.
+//! This options is mostly required when performing analysis steps afterwards without saving the
+//! full simulation results.
+//! See [SledStorageInterface](crate::storage::SledStorageInterface)
+
 /// Common interface for all storage solutions.
 mod concepts;
 
