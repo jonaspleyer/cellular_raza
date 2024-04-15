a19c08c3a1b23d8b82ecefb072c7307f55571cca
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-15 14:01:55 +0200

    auto-detect most recent folder

--- cellular_raza-examples/kidney_organoid_model/generate_movie.sh
+++ cellular_raza-examples/kidney_organoid_model/generate_movie.sh
@@ -1,8 +1,13 @@
 # Generated with ffmpeg 6.1.1
 
+FOLDERS=($(ls "out/kidney_organoid_model"))
+LAST_FOLDER=${FOLDERS[-1]}
+
+echo $LAST_FOLDER
+
 ffmpeg \
     -y \
     -pattern_type glob \
-    -i 'out/kidney_organoid_model/images/*.png' \
+    -i 'out/kidney_organoid_model/'$LAST_FOLDER/'images/*.png' \
     -c:v libx264 \
     -pix_fmt yuv420p output.mp4
