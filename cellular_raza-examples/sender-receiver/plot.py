67fb95c4f7b8400823ed392091f358270517eee0
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-17 12:14:07 +0200

    temporarily disable descriptions of plots

--- cellular_raza-examples/sender-receiver/plot.py
+++ cellular_raza-examples/sender-receiver/plot.py
@@ -6,17 +6,17 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots(2, 2)
 
-    descr = [
-        "du",
-        "proportional",
-        "differential",
-        "integral",
-    ]
+    # descr = [
+    #     "du",
+    #     "proportional",
+    #     "differential",
+    #     "integral",
+    # ]
     
     for i in range(2):
         for j in range(2):
             ax[i,j].plot(data[:,j*2+i])
-            ax[i,j].set_title(descr[j*2+i])
+            # ax[i,j].set_title(descr[j*2+i])
 
     fig.tight_layout()
     fig.savefig("controller_logs.png")
