6f7bc47d3fc211b9603d4540eb1a462faedbf072
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-27 15:18:07 +0200

    create nicer plot for controller

--- cellular_raza-examples/sender-receiver/plot.py
+++ cellular_raza-examples/sender-receiver/plot.py
@@ -12,8 +12,8 @@ def plot_pid_controller():
     data = np.genfromtxt(last_run_dir / "pid_controller.csv", delimiter=",")
 
     fig, ax = plt.subplots(
-        3, 2,
-        figsize=(12,8),
+        2, 2,
+        figsize=(8,8),
         sharex=True,
         sharey='row',
         gridspec_kw={"wspace":0, "hspace":0}
@@ -32,9 +32,16 @@ def plot_pid_controller():
         for j in range(3):
             if data.shape[1] > j*2+i:
                 if j*2+i > 0:
-                    ax[j, i].plot(0.0 * data[:,j*2+i], color="grey", linestyle="--")
-                ax[j,i].plot(data[:,j*2+i])
-                ax[j,i].set_title(descr[j*2+i])
+                    ax[min(j,1), i].plot(0.0 * data[:,j*2+i], color="grey", linestyle="--")
+                if j*2+i == 3:
+                    ax[min(j, 1), 0].plot(data[:,j*2+i], label=descr[j*2+i])
+                    ax[min(j, 1), 0].legend()
+                elif j*2+i == 0:
+                    ax[0,0].plot(2 + 0*data[:,0], color="grey", linestyle="--")
+                    ax[min(j, 1), i].plot(data[:,j*2+i], label=descr[j*2+i])
+                else:
+                    ax[min(j, 1), i].plot(data[:,j*2+i], label=descr[j*2+i])
+                ax[min(j, 1), i].legend()
 
     fig.tight_layout()
     fig.savefig(last_run_dir / "pid_controller.png")
@@ -43,9 +50,26 @@ def plot_pid_controller():
     plt.show()
     plt.close(fig)
 
+def plot_exponential_controller():
+    pass
+
+def plot_delay_ode_controller():
+    pass
+
 if __name__ == "__main__":
     try:
         plot_pid_controller()
         print("[x] pid_controller")
     except:
         print("[ ] pid_controller")
+    try:
+        plot_exponential_controller()
+        print("[x] exponential_controller")
+    except:
+        print("[ ] exponential_controller")
+    try:
+        plot_delay_ode_controller()
+        print("[x] delay_ode_controller")
+    except:
+        print("[ ] delay_ode_controlle")
+
