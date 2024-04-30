623ed10f0dcb8916e798a331d2fc90e0ad521330
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-04-30 05:16:15 +0200

    add function for plotting of delay ode controller

--- cellular_raza-examples/sender-receiver/plot.py
+++ cellular_raza-examples/sender-receiver/plot.py
@@ -54,7 +54,34 @@ def plot_exponential_controller():
     pass
 
 def plot_delay_ode_controller():
-    pass
+    last_run_dir = get_last_run_dir()
+    data = np.genfromtxt(last_run_dir / "delay_ode_mpc.csv", delimiter=",")
+
+    descr = [
+        "average concentration",
+        "cost",
+        "predicted production term",
+        "predicted concentration",
+    ]
+
+    # Create plotting base canvas
+    fig, ax = plt.subplots(2, 2)
+
+    # Plot all exported data
+    for i in range(2):
+        for j in range(2):
+            ax[j,i].plot(data[:,j*2+i], label=descr[j*2+i])
+            ax[j,i].legend()
+
+    fig.tight_layout()
+
+    # Save to files
+    fig.savefig(last_run_dir / "delay_ode_mpc.png")
+    fig.savefig(last_run_dir / "delay_ode_mpc.pdf")
+
+    # Close plots and (hopefully) free memory
+    plt.show()
+    plt.close(fig)
 
 if __name__ == "__main__":
     try:
