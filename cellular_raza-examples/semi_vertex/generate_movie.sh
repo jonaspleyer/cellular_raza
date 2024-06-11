140d0de0fcac613989bc81810647f4cc71f89e90
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-06-11 14:26:08 +0200

    generate movie in output folder directly

--- cellular_raza-examples/semi_vertex/generate_movie.sh
+++ cellular_raza-examples/semi_vertex/generate_movie.sh
@@ -8,6 +8,6 @@ python plot.py
 ffmpeg \
     -y \
     -pattern_type glob \
-    -i 'out/semi_vertex/'$LAST_FOLDER/'images/*.png' \
+    -i "out/semi_vertex/"$LAST_FOLDER'/images/*.png' \
     -c:v libx264 \
-    -pix_fmt yuv420p output.mp4
+    -pix_fmt yuv420p "out/semi_vertex/"$LAST_FOLDER'/output.mp4'
