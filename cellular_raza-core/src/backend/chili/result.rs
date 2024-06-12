8debab33959376e79721ca8316a2e3ef3e4f8e27
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-06-12 03:18:55 +0200

    add methods to quickly retrieve save path of storage result

--- cellular_raza-core/src/backend/chili/result.rs
+++ cellular_raza-core/src/backend/chili/result.rs
@@ -1,4 +1,4 @@
-use super::{CellIdentifier, VoxelPlainIndex};
+use super::{CellIdentifier, SimulationError, VoxelPlainIndex};
 use crate::storage::StorageManager;
 
 /// Gathers the [StorageManager] for cells and voxels of the previously run simulation
@@ -7,4 +7,16 @@ pub struct StorageAccess<C, V> {
     pub cells: StorageManager<CellIdentifier, C>,
     /// Access voxels at their saved iteration steps
     pub voxels: StorageManager<VoxelPlainIndex, V>,
+}
+
+impl<C, V> StorageAccess<C, V> {
+    /// Obtain the save path for cells and voxels of this simulation
+    pub fn get_path(&self) -> Result<std::path::PathBuf, SimulationError> {
+        match self.cells.extract_builder().get_full_path().parent() {
+            Some(p) => Ok(p.to_path_buf()),
+            None => Err(SimulationError::IoError(std::io::Error::from(
+                std::io::ErrorKind::NotFound,
+            ))),
+        }
+    }
 }
