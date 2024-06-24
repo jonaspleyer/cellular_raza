5df502a607fd618ab01975ff1532a6f8cffc9699
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-06-24 17:11:20 +0200

    partially revert deprecation

--- cellular_raza-building-blocks/src/domains/mod.rs
+++ cellular_raza-building-blocks/src/domains/mod.rs
@@ -1,11 +1,11 @@
 mod cartesian_cuboid_n;
 
 /// Contains deprecated cartesian cuboid implementations for an older vertex model
-#[allow(deprecated)]
+// TODO #[allow(deprecated)]
 pub mod cartesian_cuboid_2_vertex_old;
 
 /// Contains deprecated cartesian cuboid implementations
-#[allow(deprecated)]
+// TODO #[allow(deprecated)]
 pub mod cartesian_cuboid_n_old;
 
 pub use cartesian_cuboid_n::*;
