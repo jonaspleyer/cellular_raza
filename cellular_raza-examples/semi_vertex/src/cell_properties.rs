01e6689a52567479f0b7871e4c9df18f6bdf3373
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-07-27 05:42:01 +0200

    implement reaction diffusion system and simplify the contact reactions equations

--- cellular_raza-examples/semi_vertex/src/cell_properties.rs
+++ cellular_raza-examples/semi_vertex/src/cell_properties.rs
@@ -28,38 +28,41 @@ pub struct MyCell<const D: usize> {
     pub mechanics: VertexMechanics2D<D>,
     #[Interaction]
     pub interaction: VertexDerivedInteraction<OutsideInteraction, InsideInteraction>,
-    pub intracellular: Vector2<f64>,
+    pub intracellular: nalgebra::Vector3<f64>,
     pub k1: f64,
     pub k2: f64,
     pub k3: f64,
-    pub exchange: Vector2<f64>,
+    pub k4: f64,
+    pub k5: f64,
+    pub contact_range: f64,
 }
 
-impl<const D: usize> ReactionsContact<Vector2<f64>, nalgebra::SMatrix<f64, D, 2>> for MyCell<D> {
+impl<const D: usize> ReactionsContact<nalgebra::Vector3<f64>, nalgebra::SMatrix<f64, D, 2>>
+    for MyCell<D>
+{
     fn get_contact_information(&self) -> () {}
+
     fn calculate_contact_increment(
         &self,
-        own_intracellular: &Vector2<f64>,
-        ext_intracellular: &Vector2<f64>,
+        own_intracellular: &nalgebra::Vector3<f64>,
+        ext_intracellular: &nalgebra::Vector3<f64>,
         own_pos: &nalgebra::SMatrix<f64, D, 2>,
         ext_pos: &nalgebra::SMatrix<f64, D, 2>,
         _rinf: &(),
-    ) -> Result<(Vector2<f64>, Vector2<f64>), CalcError> {
-        use itertools::Itertools;
-        // Calculate overlap of borders
-        let mut incr = Vector2::zeros();
-        for (own_l1, own_l2) in own_pos.row_iter().circular_tuple_windows::<(_, _)>() {
-            for (ext_l1, ext_l2) in ext_pos.row_iter().circular_tuple_windows() {
-                let middle_own = 0.5 * (own_l1 + own_l2);
-                let middle_ext = 0.5 * (ext_l1 + ext_l2);
-                if (middle_own - middle_ext).norm()
-                    < self.interaction.outside_interaction.interaction_range * 0.5
-                {
-                    incr.x += self.exchange.x * (ext_intracellular - own_intracellular).x;
-                    incr.y += self.exchange.y * (ext_intracellular - own_intracellular).y;
-                }
-            }
+    ) -> Result<(nalgebra::Vector3<f64>, nalgebra::Vector3<f64>), CalcError> {
+        let middle_own = own_pos.row_mean();
+        let middle_ext = ext_pos.row_mean();
+        let r = (middle_own - middle_ext).norm();
+        if r < self.contact_range {
+            let incr = [
+                self.k3 * (ext_intracellular - own_intracellular)[0],
+                0.0,
+                0.0,
+            ]
+            .into();
+            return Ok((incr, -incr));
         }
+        let incr = [0.0; 3].into();
         Ok((incr, -incr))
     }
 }
@@ -114,25 +117,27 @@ impl Interaction<Vector2<f64>, Vector2<f64>, Vector2<f64>> for InsideInteraction
     fn get_interaction_information(&self) -> () {}
 }
 
-impl<const N: usize> Intracellular<Vector2<f64>> for MyCell<N> {
-    fn get_intracellular(&self) -> Vector2<f64> {
+impl<const N: usize> Intracellular<nalgebra::Vector3<f64>> for MyCell<N> {
+    fn get_intracellular(&self) -> nalgebra::Vector3<f64> {
         self.intracellular
     }
 
-    fn set_intracellular(&mut self, intracellular: Vector2<f64>) {
+    fn set_intracellular(&mut self, intracellular: nalgebra::Vector3<f64>) {
         self.intracellular = intracellular;
     }
 }
 
-impl<const N: usize> Reactions<Vector2<f64>> for MyCell<N> {
+impl<const N: usize> Reactions<nalgebra::Vector3<f64>> for MyCell<N> {
     fn calculate_intracellular_increment(
         &self,
-        intracellular: &Vector2<f64>,
-    ) -> Result<Vector2<f64>, CalcError> {
-        let a = intracellular.x;
-        let s = intracellular.y;
-        let da = self.k1 * s * a.powf(2.0) - self.k2 * a;
-        let ds = -self.k1 * s * a.powf(2.0) + self.k3;
-        Ok([da, ds].into())
+        intracellular: &nalgebra::Vector3<f64>,
+    ) -> Result<nalgebra::Vector3<f64>, CalcError> {
+        let ttgl = intracellular[0];
+        let gl3 = intracellular[1];
+        let ac = intracellular[2];
+        let dttgl = self.k1 - self.k2 * ttgl - ttgl * gl3;
+        let dgl3 = self.k4 * ac.powf(2.0) - self.k5 * gl3 - ttgl * gl3;
+        let dac = ttgl * gl3 - ac;
+        Ok([dttgl, dgl3, dac].into())
     }
 }
