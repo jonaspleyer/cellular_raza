4485aa15262897562232f405fdd1adf6a9f7bc30
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-08-22 17:19:26 +0200

    add detailed description for function calls generated by the run_main macro

--- cellular_raza-core/src/backend/mod.rs
+++ cellular_raza-core/src/backend/mod.rs
@@ -38,9 +38,122 @@ pub mod cpu_os_threads;
 
 /// 🌶️ A modular, reusable, general purpose backend
 ///
-/// This backend delivers a modular approach to building a fully working simulation.
-/// In the future it will take over the role of the [cpu_os_threads]
-/// backend as the default backend.
+/// # Overview
+/// TODO formulate in short words how this backend works
+///
+/// # Simulation Flow
+/// The [run_simulation](crate::backend::chili::run_simulation) macro (which calls the
+/// [run_main](crate::backend::chili::run_main) macro) generates a set of functions
+/// depending on the choice of simulation aspects.
+/// Below, we show a list of all these functions and their corresponding aspects.
+///
+/// | Aspects | Function | Purpose |
+/// | --- | --- | --- |
+#[doc = "\
+    | `Mechanics && Interaction`\
+    | [update_mechanics_interaction_step_1](chili::SubDomainBox::update_mechanics_interaction_step_1)\
+    | Send [PosInformation](chili::PosInformation) between threads to get back \
+      [ForceInformation](chili::ForceInformation) |"
+]
+#[doc = "\
+    | `DomainForce`\
+    | [calculate_custom_domain_force](chili::SubDomainBox::calculate_custom_domain_force)\
+    | Uses the [SubDomainForce](cellular_raza_concepts::SubDomainForce) trait to add \
+      custom external force. |"
+]
+#[doc = "\
+    | `ReactionsContact`\
+    | [update_contact_reactions_step_1]\
+      (chili::SubDomainBox::update_contact_reactions_step_1) \
+    | Sends [ReactionsContactInformation](chili::ReactionsContactInformation) between threads. |"
+]
+#[doc = "\
+    | | [sync](chili::SubDomainBox::sync) | Wait for threads to have finished until proceeding. |"
+]
+#[doc = "\
+    | `ReactionsExtra` \
+    | [update_reactions_extra_step_1](chili::SubDomainBox::update_reactions_extra_step_1) \
+    | Sends [ReactionsExtraBorderInfo](chili::ReactionsExtraBorderInfo) between threads. |"
+]
+#[doc = "\
+    | `Mechanics && Interaction` \
+    | [update_mechanics_interaction_step_2](chili::SubDomainBox::update_mechanics_interaction_step_2) \
+    | Calculate forces and return [ForceInformation](chili::ForceInformation) to the original \
+      sender. |"
+]
+#[doc = "\
+    | `ReactionsContact` \
+    | [update_contact_reactions_step_2](chili::SubDomainBox::update_contact_reactions_step_2) \
+    | Calculates the combined increment and returns \
+      [ReactionsContactReturn](chili::ReactionsContactReturn) |"
+]
+#[doc = "\
+    | `ReactionsExtra` \
+    | [update_reactions_extra_step_2](chili::SubDomainBox::update_reactions_extra_step_2) \
+    | Returns [ReactionsExtraBorderReturn](chili::ReactionsExtraBorderReturn) |"
+]
+#[doc = "\
+    | \
+    | [sync](chili::SubDomainBox::sync) \
+    | Wait for threads to have finished until proceeding. |"
+]
+#[doc = "\
+    | `Mechanics && Interaction` \
+    | [update_mechanics_interaction_step_3](chili::SubDomainBox::update_mechanics_interaction_step_3) \
+    | Receives the [ForceInformation](chili::ForceInformation) and adds within the \
+      `aux_storage`. |"
+]
+#[doc = "\
+    | `ReactionsContact` \
+    | [update_contact_reactions_step_3](chili::SubDomainBox::update_contact_reactions_step_3) \
+    | Receives the [ReactionsContactReturn](chili::ReactionsContactReturn) and adds within the `aux_storage`. |"
+]
+#[doc = "\
+    | `ReactionsExtra` \
+    | [update_reactions_extra_step_3](chili::SubDomainBox::update_reactions_extra_step_3) \
+    | Receives the [ReactionsExtraBorderReturn](chili::ReactionsExtraBorderReturn). |"
+]
+#[doc = "\
+    | \
+    | [sync](chili::SubDomainBox::sync) \
+    | Wait for threads to have finished until proceeding. |"
+]
+#[doc = "\
+    | `ReactionsExtra` \
+    | [local_subdomain_update_reactions_extra](chili::SubDomainBox::local_subdomain_update_reactions_extra) \
+    | Perform the update of the extracellular reactions. |"
+]
+#[doc = "\
+    | `Mechanics` \
+    | [local_mechanics_update_step_3](chili::local_mechanics_update_step_3) \
+    | Performs numerical integration of the position and velocity. |"
+]
+#[doc = "\
+    | `Interaction` \
+    | [local_interaction_react_to_neighbors](chili::local_interaction_react_to_neighbors) \
+    | Performs changes due to neighbor counting. |"
+]
+#[doc = "\
+    | `Cycle` \
+    | [local_cycle_update](chili::local_cycle_update) \
+    | Advance the cycle of the cell. This may introduce [CycleEvent](cellular_raza_concepts::CycleEvent) |"
+]
+#[doc = "\
+    | `Mechanics` \
+    | [local_mechanics_set_random_variable](chili::local_mechanics_set_random_variable) \
+    | Sets the random variable used for stochastic mechanical processes. |"
+]
+#[doc = "\
+    | `Reactions` \
+    | [local_reactions_intracellular](chili::local_reactions_intracellular) \
+    | Calculates increment from purely intracellular reactions. |"
+]
+#[doc = "\
+    | `ReactionsContact` \
+    | [local_reactions_intracellular](chili::local_reactions_intracellular) \
+    | Calculates increment from purely intracellular reactions. |"
+]
+///
 pub mod chili;
 
 /// 🐯 GPU-centered backend using [OpenCL](https://www.khronos.org/opencl/)
