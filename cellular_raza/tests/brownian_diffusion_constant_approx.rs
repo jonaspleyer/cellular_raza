392d80f1ed4736e5d2be0fd4f3120d838d349041
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-08-28 10:40:46 +0200

    always use smaller save interval

--- cellular_raza/tests/brownian_diffusion_constant_approx.rs
+++ cellular_raza/tests/brownian_diffusion_constant_approx.rs
@@ -25,9 +25,6 @@ impl Default for Parameters {
 
             dt: 1e-3,
             n_steps: 100,
-            #[cfg(debug_assertions)]
-            save_interval: 10,
-            #[cfg(not(debug_assertions))]
             save_interval: 1,
 
             diffusion_constant: 1.0,
