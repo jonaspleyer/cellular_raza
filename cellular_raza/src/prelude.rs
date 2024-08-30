ebcc1264685d08a1b2182eb71a2680e21da3036b
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-08-30 15:47:45 +0200

    also export time

--- cellular_raza/src/prelude.rs
+++ cellular_raza/src/prelude.rs
@@ -2,4 +2,5 @@ pub use cellular_raza_building_blocks::*;
 pub use cellular_raza_concepts::*;
 pub use cellular_raza_core::backend::chili::*;
 pub use cellular_raza_core::storage::*;
+pub use cellular_raza_core::time::*;
 pub use cellular_raza_core::*;
