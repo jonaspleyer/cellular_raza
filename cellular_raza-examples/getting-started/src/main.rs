401c8946c6cd7f5b59ca5751be9f35e8b135504d
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-09-03 17:40:48 +0200

    move from NewonDamped2D to Langevin mechanics; also adjust parameters

--- cellular_raza-examples/getting-started/src/main.rs
+++ cellular_raza-examples/getting-started/src/main.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 #[derive(CellAgent, Clone, Deserialize, Serialize)]
 struct Agent {
     #[Mechanics]
-    mechanics: NewtonDamped2D,
+    mechanics: Langevin2D,
     #[Interaction]
     interaction: MorsePotential,
 }
@@ -15,7 +15,7 @@ struct Agent {
 struct Parameters {
     domain_size: f64,
     cell_number: usize,
-    cell_mechanics: NewtonDamped2D,
+    cell_mechanics: Langevin2D,
     cell_interaction: MorsePotential,
     time_dt: f64,
     time_save_interval: f64,
@@ -27,25 +27,25 @@ struct Parameters {
 impl Default for Parameters {
     fn default() -> Self {
         Self {
-            domain_size: 100.0,
-            cell_number: 20,
-            cell_mechanics: NewtonDamped2D {
-                pos: [0.0; 2].into(),
-                vel: [0.0; 2].into(),
-                damping_constant: 0.1,
-                mass: 1.0,
+            domain_size: 100.0, // µm
+            cell_number: 60,
+            cell_mechanics: Langevin2D {
+                pos: [0.0; 2].into(), // µm
+                vel: [0.0; 2].into(), // µm
+                damping: 2.0,         // 1/min
+                mass: 1.0,            // picogram
+                kb_temperature: 0.3,  // picogram µm^2 / min^2
             },
             cell_interaction: MorsePotential {
-                length_repelling: 5.0,
-                length_attracting: 10.0,
-                strength_repelling: 3.0,
-                strength_attracting: 0.5,
-                cutoff: 15.0,
+                radius: 2.0,          // µm
+                potential_width: 1.0, // 1/µm
+                cutoff: 6.0,          // µm
+                strength: 0.01,       // picogram * µm / min^2
             },
-            time_dt: 0.1,
-            time_save_interval: 1.0,
-            time_start: 0.0,
-            time_end: 10_000.0,
+            time_dt: 0.01,           // min
+            time_save_interval: 1.0, // min
+            time_start: 0.0,         // min
+            time_end: 1_000.0,       // min
             n_threads: 1.try_into().unwrap(),
         }
     }
@@ -57,20 +57,19 @@ fn main() -> Result<(), SimulationError> {
     // Define the seed
     let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(1);
 
-    let cells = (0..parameters.cell_number)
-        .map(|_| {
-            let pos = [
-                rng.gen_range(0.0..parameters.domain_size),
-                rng.gen_range(0.0..parameters.domain_size),
-            ];
-            Agent {
-                mechanics: NewtonDamped2D {
-                    pos: pos.into(),
-                    ..parameters.cell_mechanics.clone()
-                },
-                interaction: parameters.cell_interaction.clone(),
-            }
-        });
+    let cells = (0..parameters.cell_number).map(|_| {
+        let pos = [
+            rng.gen_range(0.0..parameters.domain_size),
+            rng.gen_range(0.0..parameters.domain_size),
+        ];
+        Agent {
+            mechanics: Langevin2D {
+                pos: pos.into(),
+                ..parameters.cell_mechanics.clone()
+            },
+            interaction: parameters.cell_interaction.clone(),
+        }
+    });
 
     let domain = CartesianCuboid2New::from_boundaries_and_interaction_ranges(
         [0.0; 2],
