030b0ba7252f545116bc2e882d2e0c921a1104ef
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-18 23:37:37 +0200

    remove prelude file from building blocks

--- cellular_raza-building-blocks/src/prelude.rs
+++
@@ -1,8 +0,0 @@
-pub use crate::cell_building_blocks::cycle::*;
-pub use crate::cell_building_blocks::interaction::*;
-pub use crate::cell_building_blocks::mechanics::*;
-
-pub use crate::cell_models::modular_cell::*;
-
-pub use crate::domains::cartesian_cuboid_2_vertex::*;
-pub use crate::domains::cartesian_cuboid_n::*;
