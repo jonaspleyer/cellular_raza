e4e34ddd1f42eb2d7a4d06d8563e0e5ddc86b6f2
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-28 13:51:13 +0100

    script to generate individual CI workflow files

---
+++ .github/workflows/generate_tests.py
@@ -0,0 +1,21 @@
+oss = ["ubuntu-latest", "macos-12", "macos-14", "windows-latest"]
+toolchains = ["nightly", "beta", "stable"]
+
+
+for toolchain in toolchains:
+    for os in oss:
+        filename = "test_{}_{}.yml".format(toolchain, os)
+        contents = f'''\
+on: [push, pull_request]
+
+name: Test-Suite {toolchain} {os}
+
+jobs:
+  CI-{toolchain}-{os}:
+    uses: ./.github/workflows/reuse.yml
+    with:
+      toolchain: {toolchain}
+      os: {os}
+'''
+        with open(filename, "w") as f:
+            f.write(contents)
