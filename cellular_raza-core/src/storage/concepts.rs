d5e109376f2d26eca9371a4ec4a6a067345824d1
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-11-04 15:10:48 +0100

    use uniquevec crate which is an extended copy of the code developed here

--- cellular_raza-core/src/storage/concepts.rs
+++ cellular_raza-core/src/storage/concepts.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
+use uniquevec::UniqueVec;
 
 #[cfg(feature = "tracing")]
 use tracing::instrument;
@@ -145,114 +146,6 @@ pub enum StorageOption {
     Ron,
     /// A [std::collections::HashMap](HashMap) based memory storage.
     Memory,
-}
-
-/// A unique vector containing only non-recurring values but in the correct order.
-///
-/// ```
-/// # use cellular_raza_core::storage::UniqueVec;
-/// let mut unique_vec = UniqueVec::new();
-/// unique_vec.push(1_usize);
-/// unique_vec.push(2_usize);
-/// let res = unique_vec.push(1_usize);
-/// assert!(res.is_some());
-/// assert_eq!(*unique_vec, vec![1, 2]);
-/// ```
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct UniqueVec<T>(Vec<T>);
-
-impl<T> UniqueVec<T> {
-    /// Creates an new empty [UniqueVec].
-    pub fn new() -> Self {
-        Self(Vec::new())
-    }
-
-    /// Construct a new [UniqueVec] from a given vector.
-    /// This function will also return the rest which was not inserted into the [UniqueVec].
-    ///
-    /// ```
-    /// # use cellular_raza_core::storage::UniqueVec;
-    /// let input = vec![1, 33, 2, 0, 33, 4, 56, 2];
-    /// let (unique_vec, rest) = UniqueVec::from_iter(input);
-    /// assert_eq!(*unique_vec, vec![1, 33, 2, 0, 4, 56]);
-    /// assert_eq!(rest, vec![33, 2]);
-    /// ```
-    pub fn from_iter(iter: impl IntoIterator<Item = T>) -> (Self, Vec<T>)
-    where
-        T: PartialEq,
-    {
-        let mut new_inner = Vec::new();
-        let rest = iter
-            .into_iter()
-            .filter_map(|element| {
-                if new_inner.contains(&element) {
-                    Some(element)
-                } else {
-                    new_inner.push(element);
-                    None
-                }
-            })
-            .collect();
-        (Self(new_inner), rest)
-    }
-
-    /// Add an element to the [UniqueVec] if not already present.
-    ///
-    /// ```
-    /// # use cellular_raza_core::storage::UniqueVec;
-    /// let mut unique_vec = UniqueVec::new();
-    /// assert!(unique_vec.push(1_f64).is_none());
-    /// assert!(unique_vec.push(2_f64).is_none());
-    /// assert!(unique_vec.push(1_f64).is_some());
-    /// assert_eq!(*unique_vec, vec![1_f64, 2_f64]);
-    /// ```
-    pub fn push(&mut self, element: T) -> Option<T>
-    where
-        T: PartialEq,
-    {
-        if self.0.contains(&element) {
-            Some(element)
-        } else {
-            self.0.push(element);
-            None
-        }
-    }
-
-    /// Empties the [UniqueVec] returning all values
-    pub fn clear(&mut self) {
-        self.0.clear()
-    }
-
-    /// Remove last element from [UniqueVec]
-    pub fn pop(&mut self) -> Option<T> {
-        self.0.pop()
-    }
-}
-
-impl<T> core::ops::Deref for UniqueVec<T> {
-    type Target = Vec<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<T> From<Vec<T>> for UniqueVec<T>
-where
-    T: PartialEq,
-{
-    fn from(value: Vec<T>) -> Self {
-        Self::from_iter(value).0
-    }
-}
-
-impl<T> IntoIterator for UniqueVec<T> {
-    type Item = T;
-    type IntoIter = <Vec<T> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
 }
 
 impl StorageOption {
