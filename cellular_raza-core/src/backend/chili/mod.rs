31d9f01379c41049759296d19cbdc0e30b94e213
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-11-16 00:46:23 +0100

    fix typo

--- cellular_raza-core/src/backend/chili/mod.rs
+++ cellular_raza-core/src/backend/chili/mod.rs
@@ -147,7 +147,7 @@
 //! #     assert_eq!(cells.len(), 10);
 //! #     for (_, (cbox, _)) in cells {
 //! #         let calculated = cbox.get_id().1 as f64 + iteration as f64 * 0.1 * initial_vel;
-//! #         let cr = cbox.cell.0;
+//! #         let cr = cbox.cell.pos;
 //! #         assert!((calculated - cr).abs() < 1e-5);
 //! #     }
 //! }
