3f04d46181ab91253d55e21973dc5865a23fb734
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-23 23:20:04 +0100

    add more experimental code for cara backend

--- cellular_raza-core/src/backend/cara/cara.cu
+++ cellular_raza-core/src/backend/cara/cara.cu
@@ -2,7 +2,7 @@
 #include <cinttypes>
 #include <cstdio>
 
-typedef uint32_t UID;
+typedef uint64_t UID;
 
 struct Position {
     float p[3];
@@ -24,30 +24,85 @@ struct Agent {
 
 struct AgentContainer {
     Agent agent;
-    Force current_force;
 };
 
 struct Runner {
-    uint32_t n_agents;
+    /// Counter to assign unique identifiers to new agents
+    uint64_t uid_counter;
+
+    /// Number of agents currently stored
+    uint64_t n_agents;
+
+    /// Values of the Agents are fully updated.
+    bool agents_up_to_date;
+
     // This is a pointer to a single element which marks the first element of an array.
     // The length of this array is given by the previous value.
-    Agent *agents;
+    AgentContainer *agents;
+
+    /// Pointer to an array of floats which stores the positions of the agents
+    float *positions;
+
+    /// Pointer to an array of floats which stores the velocities of the agents
+    float *velocities;
+
+    /// Pointer to an array of floats which stores the forces of the agents
+    float *forces;
+
+    /// Function pointer which calculates the interaction force between two agents.
+    // Force (*fptr)(Position, Velocity, Position, Velocity);
 };
 
+__global__ void calculate_forces(int total, float *pos1, float *vel1, float *pos2, float *vel2,
+                                 float *forces) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i >= total || j >= total) {
+        return;
+    }
+
+    float p1, p2;
+    // float v1, v2;
+    p1 = pos1[i];
+    // v1 = vel1[i];
+    p2 = pos2[j];
+    // v2 = vel2[j];
+
+    forces[i] += 0.1 * (p1 - p2);
+    forces[j] += 0.1 * (p2 - p1);
+}
+
+/// This calculates with time complexity O(n^2) which is not ideal but the most simple thing that
+// one can write down so we will stick with it for now.
 extern "C" void update_positions(Runner &runner) {
-    for (uint32_t i = 0; i < runner.n_agents; i++) {
-        for (uint32_t j = 0; j < runner.n_agents; j++) {
+    for (uint64_t i = 0; i < runner.n_agents; i++) {
+        for (uint64_t j = 0; j < runner.n_agents; j++) {
+            float p1[3] = {runner.positions[i], runner.positions[i + 1], runner.positions[i + 2]};
+            /* float v1[3] = {runner.velocities[i], runner.velocities[i + 1],
+                           runner.velocities[i + 2]};*/
+            float p2[3] = {runner.positions[j], runner.positions[j + 1], runner.positions[j + 2]};
+            /* float v2[3] = {runner.velocities[j], runner.velocities[j + 1],
+                           runner.velocities[j + 2]};*/
+
+            float f[3];
+            for (uint8_t k = 0; k < 3; k++) {
+                // Calculate the force between agents
+                f[k] += 0.1 * (p1 - p2);
+
+                // Update forces
+                runner.forces[i + k] += f[k];
+                runner.forces[j + k] -= f[k];
+            }
         }
     }
 }
 
 extern "C" void print_positions(Runner &runner) {
-    printf("%p\n\n\n", runner.agents);
-    for (uint32_t i = 0; i < runner.n_agents; i++) {
-        Agent agent = runner.agents[i];
+    for (uint64_t i = 0; i < runner.n_agents; i++) {
+        Agent agent = runner.agents[i].agent;
         Position p  = agent.position;
         Velocity v  = agent.velocity;
-        printf("%u p=[%f, %f, %f] v=[%f, %f, %f]\n", agent.id, p.p[0], p.p[1], p.p[2], v.v[0],
+        printf("%lu p=[%f, %f, %f] v=[%f, %f, %f]\n", agent.id, p.p[0], p.p[1], p.p[2], v.v[0],
                v.v[1], v.v[2]);
     }
 }
@@ -84,7 +139,8 @@ extern "C" void do_compute(void) {
 
     float maxError = 0.0f;
     for (int i = 0; i < N; i++) {
-        maxError = max(maxError, abs(y[i] - 4.0f));
+        maxError = abs(y[i] - 4.0f);
+        printf("%f", maxError);
         assert(maxError < 0.001);
     }
 
--- cellular_raza-core/src/backend/cara/mod.rs
+++ cellular_raza-core/src/backend/cara/mod.rs
@@ -1,14 +1,19 @@
 //! 🐯 (Placeholder) GPU-centered backend using [CUDA](https://developer.nvidia.com/cuda-toolkit)
 //!
-//! This backend is currently not developed and only here to serve as a placeholder.
+//! This backend is currently in a very early development state and thus not usably at this time.
 
 #[repr(C)]
 struct Runner {
-    n_agents: u32,
-    agents: *mut Agent,
+    uid_counter: u64,
+    n_agents: u64,
+    agents_up_to_date: bool,
+    agents: *mut AgentContainer,
+    positions: *mut f32,
+    velocities: *mut f32,
+    forces: *mut f32,
 }
 
-type UID = u32;
+type UID = u64;
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug)]
@@ -40,7 +45,6 @@ struct Agent {
 #[derive(Clone, Copy, Debug)]
 struct AgentContainer {
     agent: Agent,
-    current_force: Force,
 }
 
 extern "C" {
@@ -53,29 +57,56 @@ extern "C" {
 pub fn run() {
     let n_agents = 10;
     let mut agents: Vec<_> = (0..n_agents)
-        .map(|n| Agent {
-            id: n,
-            position: Position { p: [n as f32; 3] },
-            velocity: Velocity { v: [0.; 3] },
+        .map(|n| AgentContainer {
+            agent: Agent {
+                id: n,
+                position: Position { p: [n as f32; 3] },
+                velocity: Velocity { v: [0.; 3] },
+            },
         })
         .collect();
 
     let mut runner = Runner {
+        uid_counter: 0,
         n_agents,
+        agents_up_to_date: false,
         // TODO this needs to be changed. What if there are not agents present to index?
         agents: &mut (agents[0]),
+        positions: &mut 0.0,
+        velocities: &mut 0.0,
+        forces: &mut 0.0,
     };
-    println!("{:p}", runner.agents);
 
     unsafe {
         update_positions(&mut runner);
         print_positions(&runner);
         do_compute();
     }
-    assert!(false);
 }
 
 #[test]
 fn test_main() {
     run();
+}
+
+#[test]
+fn test_cudarc() -> Result<(), Box<dyn std::error::Error>> {
+    use cudarc::driver::LaunchAsync;
+    const SIN_KERNEL: &str = include_str!("kernel.cu");
+    let dev = cudarc::driver::CudaDevice::new(0)?;
+
+    // allocate buffers
+    let inp = dev.htod_copy(vec![1.0f32; 100])?;
+    let mut out = dev.alloc_zeros::<f32>(100)?;
+    let ptx = cudarc::nvrtc::compile_ptx(SIN_KERNEL)?;
+    dev.load_ptx(ptx, "my_module", &["sin_kernel"])?;
+    let sin_kernel = dev.get_func("my_module", "sin_kernel").unwrap();
+    let cfg = cudarc::driver::LaunchConfig::for_num_elems(100);
+    unsafe { sin_kernel.launch(cfg, (&mut out, &inp, 100usize)) }?;
+    let out_host: Vec<f32> = dev.dtoh_sync_copy(&out)?;
+    for i in 0..100 {
+        assert_eq!(out_host[i], 1f32.sin());
+    }
+    // assert_eq!(out_host, [1.0; 100].map(f32::sin));
+    Ok(())
 }
