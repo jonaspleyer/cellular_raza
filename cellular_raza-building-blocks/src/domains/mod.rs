dd76efe34b15e25bd510fa9ebd3fc7cf78f7e474
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-25 13:41:22 +0200

    remove obsolete module

--- cellular_raza-building-blocks/src/domains/mod.rs
+++ cellular_raza-building-blocks/src/domains/mod.rs
@@ -1,10 +1,6 @@
 mod cartesian_2d_diffusion;
 mod cartesian_cuboid_n;
 
-/// Contains deprecated cartesian cuboid implementations for an older vertex model
-// TODO #[allow(deprecated)]
-pub mod cartesian_cuboid_2_vertex_old;
-
 /// Contains deprecated cartesian cuboid implementations
 // TODO #[allow(deprecated)]
 pub mod cartesian_cuboid_n_old;
