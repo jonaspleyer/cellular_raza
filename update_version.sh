b2fbda181729ccb555aa8ac855830c75adf691ae
Jonas Pleyer <jonas.dev@pleyer.org>
2025-09-13 13:35:43 +0200

    new release candidate

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
-OLD_VERSION='0\.3\.1-rc1'
-NEW_VERSION='0\.3\.1'
+OLD_VERSION='0\.3\.1'
+NEW_VERSION='0\.3\.2-rc1'
 
 for file in $(grep -lr $OLD_VERSION --exclude-dir target *); do
     sed -i "s/$OLD_VERSION/$NEW_VERSION/" $file
