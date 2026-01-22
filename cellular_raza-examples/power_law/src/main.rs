5016c5098a97634222a4e9ec8a9812fc351323c4
Jonas Pleyer <jonas.dev@pleyer.org>
2026-01-22 23:36:29 +0100

    add new example to model infected cells while they are proliferating

---
+++ cellular_raza-examples/power_law/src/main.rs
@@ -0,0 +1,265 @@
+use cellular_raza::prelude::*;
+
+use clap::Parser;
+use nalgebra::Vector3;
+use num::Zero;
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
+use serde::{Deserialize, Serialize};
+
+type P = nalgebra::Vector3<f64>;
+type Inf = (f64, bool);
+
+#[derive(CellAgent, Clone, Deserialize, Serialize)]
+struct Cell {
+    interaction: MorsePotential,
+    #[Mechanics]
+    mechanics: Langevin3D,
+    is_infected: bool,
+    death_range: f64,
+    infection_rate_others: f64,
+    infection_rate_self: f64,
+    neighbor_infected: usize,
+    growth_rate: f64,
+    death_rate: f64,
+    division_size: f64,
+}
+
+impl NeighborSensing<P, usize, Inf> for Cell {
+    fn accumulate_information(
+        &self,
+        own_pos: &P,
+        ext_pos: &P,
+        ext_inf: &Inf,
+        accumulator: &mut usize,
+    ) -> Result<(), CalcError> {
+        if (own_pos - ext_pos).norm() < self.death_range && ext_inf.1 {
+            *accumulator += 1;
+        }
+        Ok(())
+    }
+
+    fn react_to_neighbors(&mut self, accumulator: &usize) -> Result<(), CalcError> {
+        self.neighbor_infected = *accumulator;
+        Ok(())
+    }
+
+    fn clear_accumulator(accumulator: &mut usize) {
+        *accumulator = 0;
+    }
+}
+
+impl InteractionInformation<Inf> for Cell {
+    fn get_interaction_information(&self) -> Inf {
+        (self.interaction.radius, self.is_infected)
+    }
+}
+
+impl Interaction<P, P, P, Inf> for Cell {
+    fn calculate_force_between(
+        &self,
+        own_pos: &P,
+        own_vel: &P,
+        ext_pos: &P,
+        ext_vel: &P,
+        ext_info: &Inf,
+    ) -> Result<(P, P), CalcError> {
+        self.interaction
+            .calculate_force_between(own_pos, own_vel, ext_pos, ext_vel, &ext_info.0)
+    }
+}
+
+impl Cycle<Cell> for Cell {
+    fn update_cycle(
+        rng: &mut rand_chacha::ChaCha8Rng,
+        dt: &f64,
+        cell: &mut Cell,
+    ) -> Option<CycleEvent> {
+        let a = cell.neighbor_infected as f64;
+        // Probability to infect from others
+        let cond1 = rng.random_bool(cell.infection_rate_others * dt * a / (1.0 + a));
+        // Probability to infect by itself
+        let cond2 = rng.random_bool(cell.infection_rate_self * dt);
+
+        if cond1 || cond2 {
+            cell.is_infected = true;
+            Some(CycleEvent::PhasedDeath)
+        } else if cell.interaction.radius >= cell.division_size {
+            Some(CycleEvent::Division)
+        } else {
+            cell.interaction.radius += dt * cell.growth_rate;
+            cell.interaction.cutoff = cell.interaction.radius;
+            None
+        }
+    }
+
+    fn divide(rng: &mut rand_chacha::ChaCha8Rng, c1: &mut Cell) -> Result<Cell, DivisionError> {
+        use rand::Rng;
+        // Clone existing cell
+        let mut c2 = c1.clone();
+
+        let r = c1.interaction.radius;
+
+        // Make both cells smaller
+        c1.interaction.radius /= core::f64::consts::SQRT_2;
+        c2.interaction.radius /= core::f64::consts::SQRT_2;
+        c1.interaction.cutoff = c1.interaction.radius;
+        c2.interaction.cutoff = c1.interaction.radius;
+
+        // Generate cellular splitting direction randomly
+        let theta = rng.random_range(0.0..2.0 * core::f64::consts::PI);
+        let phi = rng.random_range(0.0..2.0 * core::f64::consts::PI);
+        let dir_vec = nalgebra::Vector3::from([
+            theta.sin() * phi.cos(),
+            theta.sin() * phi.sin(),
+            theta.cos(),
+        ]);
+
+        // Define new positions for cells
+        // It is randomly chosen if the old cell is left or right
+        let offset = dir_vec * r / std::f64::consts::SQRT_2;
+        let old_pos = c1.pos();
+
+        c1.set_pos(&(old_pos + offset));
+        c2.set_pos(&(old_pos - offset));
+
+        Ok(c2)
+    }
+
+    fn update_conditional_phased_death(
+        _: &mut rand_chacha::ChaCha8Rng,
+        dt: &f64,
+        cell: &mut Cell,
+    ) -> Result<bool, DeathError> {
+        cell.interaction.radius -= cell.death_rate * dt;
+        cell.interaction.cutoff = cell.interaction.radius;
+        Ok(cell.interaction.radius < cell.division_size / 5.0)
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value_t = 100)]
+    n_cells: usize,
+    #[arg(long, default_value_t = 400.0)]
+    domain_size: f64,
+    #[arg(long, default_value_t = 24)]
+    n_threads: usize,
+    #[arg(long, default_value_t = 0.2)]
+    time_dt: f64,
+    #[arg(long, default_value_t = 30_000)]
+    time_n: u64,
+    #[arg(long, default_value_t = 20)]
+    time_save_interval: u64,
+    #[arg(long, default_value_t = 0.5)]
+    cell_damping: f64,
+    #[arg(long, default_value_t = 0.3)]
+    cell_kb_temperature: f64,
+    #[arg(long, default_value_t = 6.0)]
+    cell_radius: f64,
+    #[arg(long, default_value_t = 0.25)]
+    cell_potential_stiffness: f64,
+    #[arg(long, default_value_t = 0.01)]
+    cell_strength: f64,
+    #[arg(long, default_value_t = 24.0)]
+    cell_death_range: f64,
+    #[arg(long, default_value_t = 0.2)]
+    cell_infection_rate_others: f64,
+    #[arg(long, default_value_t = 0.00001)]
+    cell_infection_rate_self: f64,
+    #[arg(long, default_value_t = 9.0)]
+    cell_division_size: f64,
+    #[arg(long, default_value_t = 0.1)]
+    cell_growth_rate: f64,
+    #[arg(long, default_value_t = 0.5)]
+    cell_death_rate: f64,
+}
+
+fn main() -> Result<(), SimulationError> {
+    // Define the seed
+    let mut rng = ChaCha8Rng::seed_from_u64(1);
+
+    let Args {
+        n_cells,
+        domain_size,
+        n_threads,
+        time_dt: dt,
+        time_n: n_times,
+        time_save_interval: save_interval,
+        cell_damping,
+        cell_kb_temperature,
+        cell_radius,
+        cell_potential_stiffness,
+        cell_strength,
+        cell_death_range,
+        cell_infection_rate_others,
+        cell_infection_rate_self,
+        cell_division_size,
+        cell_growth_rate,
+        cell_death_rate,
+    } = Args::parse();
+
+    let cells = (0..n_cells)
+        .map(|_| {
+            let pos = Vector3::from([
+                rng.random_range(0.0..domain_size),
+                rng.random_range(0.0..domain_size),
+                rng.random_range(0.0..domain_size),
+            ]);
+            let radius = rng.random_range(0.5 * cell_radius..cell_division_size);
+            Cell {
+                mechanics: Langevin3D {
+                    pos,
+                    vel: Vector3::zero(),
+                    mass: 1.0,
+                    damping: cell_damping,
+                    kb_temperature: cell_kb_temperature,
+                },
+                interaction: MorsePotential {
+                    radius,
+                    potential_stiffness: cell_potential_stiffness,
+                    cutoff: radius,
+                    strength: cell_strength,
+                },
+                is_infected: false,
+                death_range: cell_death_range,
+                infection_rate_others: cell_infection_rate_others,
+                infection_rate_self: cell_infection_rate_self,
+                neighbor_infected: 0,
+                division_size: cell_division_size,
+                growth_rate: cell_growth_rate,
+                death_rate: cell_death_rate,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let domain = CartesianCuboid::from_boundaries_and_interaction_range(
+        [0.0; 3],
+        [domain_size; 3],
+        3.0 * cell_radius,
+    )?;
+
+    let time = cellular_raza::core::time::FixedStepsize::from_partial_save_steps(
+        0.0,
+        dt,
+        n_times,
+        save_interval,
+    )?;
+    let storage_builder = StorageBuilder::new().location("out/cell_sorting");
+
+    let settings = cellular_raza::core::backend::chili::Settings {
+        n_threads: n_threads.try_into().unwrap(),
+        time,
+        storage: storage_builder,
+        progressbar: Some("".into()),
+    };
+
+    run_simulation!(
+        domain,
+        agents: cells,
+        settings,
+        aspects: [Mechanics, Interaction, Cycle, NeighborSensing]
+    )?;
+    Ok(())
+}
---
+++ cellular_raza-examples/power_law/src/plot.py
@@ -0,0 +1,175 @@
+import pyvista as pv
+import glob
+import json
+from pathlib import Path
+import numpy as np
+import multiprocessing as mp
+import itertools
+import tqdm
+import matplotlib.pyplot as plt
+from itertools import repeat
+
+
+def get_all_iterations(path: Path, cells_subdomains: str = "cells"):
+    return [
+        (int(iter_dir.split("/")[-1]), iter_dir)
+        for iter_dir in sorted(
+            glob.glob(str(path / "{}/json/*".format(cells_subdomains)))
+        )
+    ]
+
+
+def __get_iteration_dir(iteration: int, path: Path, cells_subdomains: str = "cells"):
+    iterations = get_all_iterations(path, cells_subdomains)
+    for iteration_int, iteration_dir in iterations:
+        if iteration_int == iteration:
+            return Path(iteration_dir)
+    return None
+
+
+def get_last_save_path(path: Path):
+    paths = sorted([p for p in glob.glob(str(path) + "/*")])
+    return Path(paths[-1])
+
+
+def get_cells_at_iteration(iteration: int, path: Path):
+    iter_dir = __get_iteration_dir(iteration, path)
+    cells = []
+    for batch in glob.glob(str(iter_dir) + "/*"):
+        with open(batch) as f:
+            d = json.load(f)
+            cells_new = d["data"]
+            cells.extend(cells_new)
+    return cells
+
+
+def get_subdomains_at_iteration(iteration: int, path: Path) -> list:
+    iter_dir = __get_iteration_dir(iteration, path, "subdomains")
+    subdomains = []
+    for single in glob.glob(str(iter_dir) + "/*"):
+        with open(single) as f:
+            d = json.load(f)
+            subdomains.append(d["element"])
+    return subdomains
+
+
+def get_min_max_from_subdomains(
+    iteration: int, path: Path
+) -> tuple[np.ndarray, np.ndarray]:
+    subdomains = get_subdomains_at_iteration(iteration, path)
+    min = np.array(subdomains[0]["domain_min"])
+    max = np.array(subdomains[0]["domain_max"])
+    return (min, max)
+
+
+def get_spheres(iteration: int, path: Path):
+    cells = get_cells_at_iteration(iteration, path)
+
+    def get_info(cell):
+        return (
+            cell["element"][0]["cell"]["mechanics"]["pos"],
+            cell["element"][0]["cell"]["interaction"]["radius"],
+            cell["element"][0]["cell"]["is_infected"],
+        )
+
+    position_radius = [get_info(ci) for ci in cells]
+    pset = pv.PolyData([np.array(x[0]) for x in position_radius])
+    pset.point_data["diameter"] = 2.0 * np.array([x[1] for x in position_radius])
+    pset.point_data["infected"] = np.array([not x[2] for x in position_radius])
+
+    sphere = pv.Sphere()
+    spheres = pset.glyph(geom=sphere, scale="diameter", orient=False)
+    return spheres
+
+
+def plot_spheres(
+    iteration: int, path: Path, opath=None, transparent_background: bool = False
+):
+    spheres = get_spheres(iteration, path)
+    domain_min, domain_max = get_min_max_from_subdomains(iteration, path)
+    dx = np.max(domain_max - domain_min)
+    middle = 0.5 * (domain_min + domain_max)
+
+    plotter = pv.Plotter(off_screen=True, window_size=[1024, 1024])
+    plotter.set_background([100, 100, 100])
+    plotter.add_mesh(
+        spheres,
+        scalars="infected",
+        cmap=["red", "green"],
+        show_edges=False,
+        show_scalar_bar=False,
+    )
+    plotter.enable_ssao(radius=12)
+    plotter.enable_anti_aliasing()
+    plotter.camera.position = (-2 * dx, -2 * dx, +2 * dx)
+    plotter.camera.focal_point = middle
+    if opath is None:
+        opath = path / "images/{:010}.png".format(iteration)
+        opath.parent.mkdir(parents=True, exist_ok=True)
+    plotter.screenshot(opath, transparent_background=transparent_background)
+    plotter.close()
+    del plotter
+    del spheres
+
+
+def __plot_spheres_helper(args_kwargs):
+    args, kwargs = args_kwargs
+    plot_spheres(*args, **kwargs)
+
+
+def plot_all_spheres(path: Path, n_threads: int = None, **kwargs: dict):
+    iterations = [it[0] for it in get_all_iterations(path)]
+    pool = mp.Pool(n_threads)
+    print("Generating Images")
+    list(
+        tqdm.tqdm(
+            pool.imap_unordered(
+                __plot_spheres_helper,
+                zip(zip(iterations, itertools.repeat(path)), itertools.repeat(kwargs)),
+            ),
+            total=len(iterations),
+        )
+    )
+
+
+def __get_cell_numbers(iteration_path):
+    iteration, path = iteration_path
+    cells = get_cells_at_iteration(iteration, path)
+    markers = [c["element"][0]["cell"]["is_infected"] for c in cells]
+    x = np.sum(markers)
+    return x, len(cells) - x
+
+
+if __name__ == "__main__":
+    last_save_path = get_last_save_path(Path("out/cell_sorting"))
+
+    mp.set_start_method("spawn")
+
+    plot_all_spheres(last_save_path, n_threads=20, transparent_background=True)
+
+    iterations = get_all_iterations(last_save_path)
+    iterations = [i[0] for i in iterations]
+
+    n_cells = []
+    with mp.Pool(28) as pool:
+        n_cells = list(
+            tqdm.tqdm(
+                pool.imap(__get_cell_numbers, zip(iterations, repeat(last_save_path))),
+                total=len(iterations),
+            )
+        )
+    n_cells = np.array(n_cells)
+
+    # Plot number of cells for each iteration
+    fig, ax = plt.subplots()
+    ax.stackplot(
+        iterations,
+        n_cells[:, 0],
+        n_cells[:, 1],
+        colors=["red", "green"],
+        labels=["Infected", "Healthy"],
+    )
+    # ax.plot(iterations, n_cells[:, 1], color="green", label="Healthy")
+    ax.legend()
+    ax.set_yscale("log")
+    plt.show()
