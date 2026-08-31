e6c7733e18ca7ddae9927822ff2c136db3201bb1
Jonas Pleyer <jonas.dev@pleyer.org>
2026-08-31 16:14:16 +0200

    use time points for images later on

--- cellular_raza-examples/extracellular-particles/src/plot_polish.py
+++ cellular_raza-examples/extracellular-particles/src/plot_polish.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
         )
 
     gs2 = GridSpec(1, 3, wspace=s, left=0, right=1, bottom=0, top=1)
-    for i, j in enumerate([1, int(len(iterations) / 4), -1]):
+    for i, j in enumerate([i1, i2, -1]):
         ax = fig2.add_subplot(gs2[i])
         ax.set_axis_off()
         plot_iteration(iterations[j], data, ax=ax)
