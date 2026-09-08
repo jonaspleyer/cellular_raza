33a4d75d21709b0d924a78c1ebba98ff46f1d4fb
Jonas Pleyer <jonas.dev@pleyer.org>
2026-09-08 23:49:10 +0200

    add final schematic; rename file

--- cellular_raza-examples/rust-fungus/scripts/example.py
+++ cellular_raza-examples/rust-fungus/scripts/example.py
@@ -8,6 +8,199 @@ import string
 import cr_rust_fungus as crf
 
 
+POS_PLANT_CELL = np.array(
+    [
+        [51.08850804, 31.5084175],
+        [51.03684374, 32.31141754],
+        [51.09340809, 33.11760559],
+        [51.16790736, 33.92134747],
+        [51.13762193, 34.70647444],
+        [50.74718319, 35.38282616],
+        [50.0839777, 35.81932956],
+        [49.29361357, 36.01024525],
+        [48.46825535, 36.04664108],
+        [47.63730791, 36.03229772],
+        [46.80526594, 36.03443942],
+        [45.9734252, 36.06687566],
+        [45.14192885, 36.09207278],
+        [44.31588987, 36.03615876],
+        [43.52649951, 35.81423527],
+        [42.85084407, 35.36718549],
+        [42.40250076, 34.7031364],
+        [42.28255641, 33.91961963],
+        [42.32090349, 33.11074379],
+        [42.37188646, 32.29685487],
+        [42.39152068, 31.48077207],
+        [42.35774109, 30.66541126],
+        [42.26051608, 29.85699799],
+        [42.15461583, 29.07591411],
+        [42.43973203, 28.39286091],
+        [43.10537175, 28.0887864],
+        [43.8693455, 28.22615695],
+        [44.66259466, 28.4006257],
+        [45.4685328, 28.52704353],
+        [46.28366925, 28.58675261],
+        [47.10225773, 28.56847437],
+        [47.9167426, 28.46817236],
+        [48.72044669, 28.29074739],
+        [49.51134214, 28.05378175],
+        [50.287681, 27.82798122],
+        [51.0563427, 27.93105536],
+        [51.59920242, 28.45268352],
+        [51.73949212, 29.20811517],
+        [51.5230743, 29.96350514],
+        [51.26336855, 30.72389781],
+    ]
+)
+
+
+def plot_schematic(ax):
+    # Pick one particular agent
+    pos = POS_PLANT_CELL
+
+    # Plot the cell
+    ax.plot(
+        [*pos[:, 0], pos[0, 0]],
+        [*pos[:, 1], pos[0, 1]],
+        marker="x",
+        color="k",
+        linestyle="--",
+    )
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    # Plot zoomed in variant
+    xmin = np.min(pos[:, 0])
+    xmax = np.max(pos[:, 0])
+    ymin = np.min(pos[:, 1])
+    ymax = np.max(pos[:, 1])
+    ds = max(xmax - xmin, ymax - ymin)
+
+    # Define spacing used for all schema
+    dss = 0.4 * ds
+
+    ax.set_xlim(xmin - 0.15 * ds, xmin + 1.15 * ds)
+    ax.set_ylim(ymin - 0.15 * ds, ymin + 1.15 * ds)
+
+    def plot_schema_1():
+        # WARNING: magic numbers!
+        n1 = 10
+        n2 = 15
+        q = np.array(pos[n1:n2])
+
+        xy = (xmin + 0.1 * ds, ymin + 0.7 * ds)
+        rect = Rectangle(xy, dss, dss, facecolor="white", edgecolor="gray", zorder=100)
+        ax.add_patch(rect)
+        ax.text(
+            xy[0] + 0.03 * ds,
+            xy[1] + dss - 0.03 * ds,
+            "i",
+            fontsize=30,
+            fontweight="semibold",
+            fontfamily="serif",
+            va="top",
+            horizontalalignment="left",
+            zorder=101,
+        )
+
+        # Expand to rectangle
+        r = 1.3 * (q - q[2]) + np.array(xy) + np.array([0.5 * dss, 0.6 * dss])
+        # Clip to rectangle
+        r[:, 0] = np.clip(r[:, 0], xy[0], xy[0] + dss)
+        ax.plot(r[:, 0], r[:, 1], color="k", linewidth=2, zorder=101, marker="o")
+
+        for i in range(len(r) - 1):
+            p = 0.5 * (r[i] + r[i + 1])
+            z = r[i] - r[i + 1]
+            dir = 0.7 * np.array([-z[1], z[0]])
+            ax.annotate(
+                "", xy=p + dir, xytext=p, arrowprops=dict(arrowstyle="->"), zorder=101
+            )
+
+    def plot_schema_2():
+        xy = (xmin + 0.7 * ds, ymin + 0.3 * ds)
+        rect = Rectangle(xy, dss, dss, facecolor="white", edgecolor="gray", zorder=100)
+        ax.add_patch(rect)
+        ax.text(
+            xy[0] + 0.03 * ds,
+            xy[1] + dss - 0.03 * ds,
+            "ii",
+            fontsize=30,
+            fontweight="semibold",
+            fontfamily="serif",
+            va="top",
+            horizontalalignment="left",
+            zorder=101,
+        )
+
+        # WARNING: magic numbers!
+        n1 = -1
+        n3 = 4
+        # Clip selected points to retangle
+        q = np.array([pos[n1], pos[n3]])
+        q[:, 1] = np.clip(q[:, 1], xy[1], xy[1] + dss)
+
+        with rc_context({"path.sketch": (10, 10, 1)}):
+            for i in range(len(q) - 1):
+                ax.plot(
+                    q[i : i + 2, 0],
+                    q[i : i + 2, 1],
+                    color="k",
+                    zorder=101,
+                    markersize=5,
+                )
+
+    def plot_schema_3():
+        # WARNING: magic numbers!
+        n1 = 22
+        n2 = 27
+
+        xy = (xmin - 0.1 * ds, ymin - 0.1 * ds)
+        rect2 = Rectangle(xy, dss, dss, facecolor="white", edgecolor="gray", zorder=100)
+        ax.add_patch(rect2)
+        ax.text(
+            xy[0] + dss - 0.03 * ds,
+            xy[1] + dss - 0.03 * ds,
+            "iii",
+            fontsize=30,
+            fontweight="semibold",
+            fontfamily="serif",
+            va="top",
+            horizontalalignment="right",
+            zorder=101,
+        )
+
+        q = pos[n1:n2]
+        dmid = (q[1] + q[2] + q[3]) / 3
+        dd = max(np.max(q[:, 0]) - np.min(q[:, 0]), np.max(q[:, 1]) - np.min(q[:, 1]))
+
+        # Clamp to intervals [-1, 1]
+        r = (q - dmid) / dd
+        # Expand and clamp to rectangle boundaries
+        q = dmid + 0.5 * ds * r
+        q = np.clip(q, xy, [xy[0] + dss, xy[1] + dss])
+
+        ax.plot(q[:, 0], q[:, 1], color="k", linestyle="--", linewidth=2, zorder=101)
+
+        for i in range(1, len(q) - 1):
+            p1 = q[i - 1]
+            p2 = q[i]
+            p3 = q[i + 1]
+            c1 = p1 - p2
+            c2 = p3 - p2
+
+            a1 = np.arctan2(c1[1], c1[0]) / (2 * np.pi) * 360
+            a2 = np.arctan2(c2[1], c2[0]) / (2 * np.pi) * 360
+
+            arc = Arc(p2, 1.5, 1.5, theta1=a2, theta2=a1, zorder=101)
+            ax.add_patch(arc)
+
+    plot_schema_1()
+    plot_schema_2()
+    plot_schema_3()
+
+
 def save_snapshot(
     cells,
     domain_size,
@@ -71,7 +264,8 @@ def save_snapshot(
     dx = domain_size
     ax.set_xlim(-0.01 * dx, 1.01 * domain_size)
     ax.set_ylim(-0.01 * dx, 1.01 * domain_size)
-    ax.set_axis_off()
+    ax.set_xticks([])
+    ax.set_yticks([])
 
     if fig_ax is None:
         fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
@@ -235,88 +429,9 @@ if __name__ == "__main__":
     # for iteration in tqdm(iterations):
     #     save_snapshot(iteration, settings.domain_size, result)
 
-    fig, axs = plt.subplots(1, 3, figsize=(24, 8))
+    fig, axs = plt.subplots(1, 3, figsize=(24, 8 * 1.04))
 
-    # Pick one particular agent
-    ax = axs[0]
-    plant_cell = results[-1][int(len(results[-1]) / 2)]
-    pos = plant_cell[0].position.T
-
-    ax.plot(
-        [*pos[:, 0], pos[0, 0]],
-        [*pos[:, 1], pos[0, 1]],
-        marker="x",
-        color="k",
-        linestyle="--",
-    )
-
-    # Plot zoomed in variant
-    xmin = np.min(pos[:, 0])
-    xmax = np.max(pos[:, 0])
-    ymin = np.min(pos[:, 1])
-    ymax = np.max(pos[:, 1])
-    ds = max(xmax - xmin, ymax - ymin)
-
-    ax.set_xlim(xmin - 0.12 * ds, xmin + 1.12 * ds)
-    ax.set_ylim(ymin - 0.12 * ds, ymin + 1.12 * ds)
-
-    # Select which points to display with more detail
-    sxmin = xmin + 0.7 * ds
-    symin = ymin + 0.3 * ds
-    dss = 0.4 * ds
-
-    xy = (sxmin, symin)
-    rect = Rectangle(xy, dss, dss, facecolor="white", edgecolor="gray", zorder=100)
-    ax.add_patch(rect)
-
-    # WARNING: magic numbers ahead!
-    n1 = -1
-    n3 = 4
-    # Clip selected points to retangle
-    q = np.array([pos[n1], pos[n3]])
-    q[:, 1] = np.clip(q[:, 1], symin, symin + dss)
-
-    with rc_context({"path.sketch": (10, 10, 1)}):
-        for i in range(len(q) - 1):
-            ax.plot(
-                q[i : i + 2, 0],
-                q[i : i + 2, 1],
-                color="k",
-                zorder=101,
-                markersize=5,
-            )
-    # WARNING: second time magic numbers!
-    n1 = 22
-    n2 = 27
-
-    xy = (xmin - 0.1 * ds, ymin - 0.1 * ds)
-    rect2 = Rectangle(xy, dss, dss, facecolor="white", edgecolor="gray", zorder=100)
-    ax.add_patch(rect2)
-
-    q = pos[n1:n2]
-    dmid = (q[1] + q[2] + q[3]) / 3
-    dd = max(np.max(q[:, 0]) - np.min(q[:, 0]), np.max(q[:, 1]) - np.min(q[:, 1]))
-
-    # Clamp to intervals [-1, 1]
-    r = (q - dmid) / dd
-    # Expand and clamp to rectangle boundaries
-    q = dmid + 0.5 * ds * r
-    q = np.clip(q, xy, [xy[0] + dss, xy[1] + dss])
-
-    ax.plot(q[:, 0], q[:, 1], color="k", linestyle="--", linewidth=2, zorder=101)
-
-    for i in range(1, len(q) - 1):
-        p1 = q[i - 1]
-        p2 = q[i]
-        p3 = q[i + 1]
-        c1 = p1 - p2
-        c2 = p3 - p2
-
-        a1 = np.arctan2(c1[1], c1[0]) / (2 * np.pi) * 360
-        a2 = np.arctan2(c2[1], c2[0]) / (2 * np.pi) * 360
-
-        arc = Arc(p2, 1.2, 1.2, theta1=a2, theta2=a1, zorder=101)
-        ax.add_patch(arc)
+    plot_schematic(axs[0])
 
     for ax, label in zip(axs, string.ascii_uppercase):
         ax.text(
@@ -342,4 +457,4 @@ if __name__ == "__main__":
         )
 
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0.02)
-    fig.savefig("temp.png")
+    fig.savefig("rust-fungus-mechanics.pdf")
