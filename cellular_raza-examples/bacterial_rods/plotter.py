035f8dc65ea8cfe817bd430418d913bb37d97779
Jonas Pleyer <jonas.dev@pleyer.org>
2026-09-09 23:01:15 +0200

    formatting

--- cellular_raza-examples/bacterial_rods/plotter.py
+++ cellular_raza-examples/bacterial_rods/plotter.py
@@ -10,8 +10,10 @@ import json
 import os
 import pandas as pd
 
+
 def get_last_output_path():
     return Path(sorted(glob.glob("out/*"))[-1])
+
 
 def get_all_iterations(output_path: Path | None = None):
     if output_path is None:
@@ -19,6 +21,7 @@ def get_all_iterations(output_path: Path | None = None):
     folders = glob.glob(str(output_path / "cells/json") + "/*")
     iterations = [int(Path(fi).name) for fi in folders]
     return output_path, iterations
+
 
 def load_cells_from_iteration(output_path: Path, iteration: int):
     load_path = Path(output_path) / "cells/json/{:020}".format(iteration)
@@ -32,7 +35,7 @@ def load_cells_from_iteration(output_path: Path, iteration: int):
 
     if len(df) > 0:
         # Format individual entries for easier use later on
-        df["identifier"] = df["identifier"].apply(lambda x: tuple(x))
+        # df["identifier"] = df["identifier"].apply(lambda x: tuple(x))
         df["cell.mechanics.pos"] = df["cell.mechanics.pos"].apply(
             lambda x: np.array(x[0], dtype=float).reshape(3, -1)
         )
@@ -41,6 +44,7 @@ def load_cells_from_iteration(output_path: Path, iteration: int):
         )
 
     return df
+
 
 def get_cell_meshes(iteration: int, path: Path):
     cells = load_cells_from_iteration(path, iteration)
@@ -51,19 +55,21 @@ def get_cell_meshes(iteration: int, path: Path):
     for i, p in enumerate(positions):
         meshes = []
         # Add the sphere at the first point
-        meshes.append(pv.Sphere(center=p[:,0], radius=radii[i]))
-        for j in range(max(p.shape[1]-1,0)):
+        meshes.append(pv.Sphere(center=p[:, 0], radius=radii[i]))
+        for j in range(max(p.shape[1] - 1, 0)):
             # Add a sphere for each following point
-            meshes.append(pv.Sphere(center=p[:,j+1], radius=radii[i]))
+            meshes.append(pv.Sphere(center=p[:, j + 1], radius=radii[i]))
 
             # Otherwise add cylinders
-            pos1 = p[:,j]
-            pos2 = p[:,j+1]
+            pos1 = p[:, j]
+            pos2 = p[:, j + 1]
             center = 0.5 * (pos1 + pos2)
             direction = pos2 - center
             radius = radii[i]
             height = float(np.linalg.norm(pos1 - pos2))
-            cylinder = pv.Cylinder(center, direction, radius, height)
+            cylinder = pv.Cylinder(
+                center=center, direction=direction, radius=radius, height=height
+            )
             meshes.append(cylinder)
         combined = pv.MultiBlock(meshes).combine()
         if combined is None:
@@ -75,13 +81,14 @@ def get_cell_meshes(iteration: int, path: Path):
         cell_surfaces.append((merged, growth_rate[i]))
     return cell_surfaces
 
+
 def plot_spheres(
-        iteration: int,
-        path: Path = Path("./"),
-        opath: Path | None = None,
-        overwrite: bool = False,
-        transparent_background: bool = False,
-    ):
+    iteration: int,
+    path: Path = Path("./"),
+    opath: Path | None = None,
+    overwrite: bool = False,
+    transparent_background: bool = False,
+):
     if opath is None:
         opath = path / "images/{:010}.png".format(iteration)
         opath.parent.mkdir(parents=True, exist_ok=True)
@@ -96,14 +103,14 @@ def plot_spheres(
 
     # Draw box around everything
     dx = 3e-6
-    box = pv.Box(bounds=(-dx, 200e-6+dx, 0-dx, 15e-6+dx, 0-dx, 45e-6+dx))
+    box = pv.Box(bounds=(-dx, 200e-6 + dx, 0 - dx, 15e-6 + dx, 0 - dx, 45e-6 + dx))
     plotter.add_mesh(box, style="wireframe", line_width=15)
     color_min = np.array([69, 124, 214])
     color_max = np.array([82, 191, 106])
-    for (cell, growth_rate) in cell_meshes:
+    for cell, growth_rate in cell_meshes:
         # TODO MAGIC NUMBERS
         q = growth_rate / 1e-7
-        color = (1-q) * color_min + q * color_max
+        color = (1 - q) * color_min + q * color_max
         plotter.add_mesh(
             cell,
             show_edges=False,
@@ -123,21 +130,23 @@ def plot_spheres(
     plotter.close()
     return img
 
+
 def __plot_spheres_helper(args):
     (args, kwargs) = args
     plot_spheres(*args, **kwargs)
 
+
 def plot_all_spheres(
-        path: Path,
-        n_threads: int | None = None,
-        overwrite:bool=False,
-        transparent_background: bool = False
-    ):
+    path: Path,
+    n_threads: int | None = None,
+    overwrite: bool = False,
+    transparent_background: bool = False,
+):
     iterations = [it for it in get_all_iterations(path)[1]]
     if n_threads is None:
         n_cpu = os.cpu_count()
         if n_cpu is not None and n_cpu > 2:
-            n_threads = n_cpu -2
+            n_threads = n_cpu - 2
         else:
             n_threads = 1
     args = [(it,) for it in iterations]
@@ -147,10 +156,15 @@ def plot_all_spheres(
         "transparent_background": transparent_background,
     }
     with concurrent.futures.ProcessPoolExecutor(max_workers=n_threads) as executor:
-        _ = list(tqdm.tqdm(executor.map(
-            __plot_spheres_helper,
-            zip(args, itertools.repeat(kwargs))
-        ), total=len(iterations)))
+        _ = list(
+            tqdm.tqdm(
+                executor.map(
+                    __plot_spheres_helper, zip(args, itertools.repeat(kwargs))
+                ),
+                total=len(iterations),
+            )
+        )
+
 
 # TODO actually use this
 if __name__ == "_main__":
@@ -158,25 +172,23 @@ if __name__ == "_main__":
         prog="Plotter",
         description="A plotting CLI for the springs example",
         epilog="For suggestions or bug-reports please refer to\
-            https://github.com/jonaspleyer/cellular_raza/"
+            https://github.com/jonaspleyer/cellular_raza/",
     )
     parser.add_argument(
         "-n",
         "--iteration",
         help="Specify which iteration to plot.\
             Multiple arguments are accepted as a list.\
-            If not specified, plot all iterations."
+            If not specified, plot all iterations.",
     )
     parser.add_argument(
         "-i",
         "--input",
         help="Input path of files. If not given,\
-            it will be determined automatically by searching in './out/'"
+            it will be determined automatically by searching in './out/'",
     )
     parser.add_argument(
-        "-o",
-        "--output-path",
-        help="Path where to store generated images."
+        "-o", "--output-path", help="Path where to store generated images."
     )
 
     args = parser.parse_args()
@@ -201,4 +213,3 @@ if __name__ == "__main__":
     print("Playing Movie")
     bashcmd2 = f"firefox {output_path}/movie.mp4"
     os.system(bashcmd2)
-
